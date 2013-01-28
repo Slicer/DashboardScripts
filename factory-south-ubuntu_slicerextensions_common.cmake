6781e1fa7a5030f5429a69b94a8023676b6f4f7c
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2013-01-27 23:58:33 -0500

    Add "factory-south" scripts

---
+++ factory-south-ubuntu_slicerextensions_common.cmake
@@ -0,0 +1,22 @@
+
+message("-- Including ${CTEST_INCLUDED_SCRIPT_NAME}")
+
+#-----------------------------------------------------------------------------
+# Git repository
+#-----------------------------------------------------------------------------
+set(EXTENSIONS_INDEX_GIT_REPOSITORY git://github.com/Slicer/ExtensionsIndex.git)
+
+#-----------------------------------------------------------------------------
+# Display variables
+#-----------------------------------------------------------------------------
+foreach(var EXTENSIONS_INDEX_GIT_REPOSITORY
+            )
+  message("-- ${var}: ${${var}}")
+endforeach()
+
+#-----------------------------------------------------------------------------
+# Common settings
+#-----------------------------------------------------------------------------
+set(CTEST_INCLUDED_SCRIPT_NAME ${HOSTNAME}_slicer_common.cmake)
+include(${CTEST_SCRIPT_DIRECTORY}/${CTEST_INCLUDED_SCRIPT_NAME})
+
