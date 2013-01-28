3b8aec3777af9fc97f2e58586c1e9333c07ad9e6
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2013-01-28 03:16:40 -0500

    On factory-south-win7, disable CTKAppLauncher, slicerextensions 64bit 4.2 and 32 bit slicer-itkv4 build
    There are no 32/64bits static build of qt, or 32bit regular build of qt

--- factory-south-win7.bat
+++ factory-south-win7.bat
@@ -1,7 +1,7 @@
 
 
 REM Nightly build of CTKAppLauncher vs2008 32bits
-"C:\D\Support\CMake 2.8.10.2\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-32bits_CTKAppLauncher_release_nightly.cmake" -C Release -VV -O C:\D\Logs\factory-south-win7-vs2008-32bits_CTKAppLauncher_release_nightly.txt
+REM "C:\D\Support\CMake 2.8.10.2\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-32bits_CTKAppLauncher_release_nightly.cmake" -C Release -VV -O C:\D\Logs\factory-south-win7-vs2008-32bits_CTKAppLauncher_release_nightly.txt
 
 REM Nightly build of slicer vs2008 32bits
 REM "C:\D\Support\CMake 2.8.10.2\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-32bits_slicer4_release_nightly.cmake" -C Release -VV -O C:\D\Logs\factory-south-win7-vs2008-32bits_slicer4_release_nightly.txt
@@ -31,10 +31,10 @@ REM Nightly build of slicer 4.1.1 extensions vs2008 64bits
 REM "C:\D\Support\CMake 2.8.10.2\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicerextensions_411_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicerextensions_411_release_nightly.txt
 
 REM Nightly build of slicer 4.2 extensions vs2008 64bits
-"C:\D\Support\CMake 2.8.10.2\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicerextensions_42_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicerextensions_42_release_nightly.txt
+REM "C:\D\Support\CMake 2.8.10.2\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicerextensions_42_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicerextensions_42_release_nightly.txt
 
 REM Nightly build of slicer vs2008 32bits ITKv4
-"C:\D\Support\CMake 2.8.10.2\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-32bits_slicer4-itkv4_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-32bits_slicer4-itkv4_release_nightly.txt
+REM "C:\D\Support\CMake 2.8.10.2\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-32bits_slicer4-itkv4_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-32bits_slicer4-itkv4_release_nightly.txt
 
 REM Nightly build of slicer vs2008 64bits ITKv4
 "C:\D\Support\CMake 2.8.10.2\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicer4-itkv4_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicer4-itkv4_release_nightly.txt
