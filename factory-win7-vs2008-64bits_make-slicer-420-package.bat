c2e8ad70cd1dd27753ea3dcbc9152ec60f169538
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2013-02-12 15:42:00 -0500

    Remove "factory" scripts specific to vs2008/64-bit now done on "factory-south"

--- factory-win7-vs2008-64bits_make-slicer-420-package.bat
+++
@@ -1,3 +0,0 @@
-
-REM Build Slicer 4.1.1 package using vs2008 64bits
-"C:\D\Support\cmake-2.8.8-win32-x86\bin\ctest.exe" -S "C:\Dashboards\DashboardScripts\factory-win7-vs2008-64bits_slicer_420_release_package.cmake" -C Release -V -VV -O C:\D\Logs\factory-win7-vs2008-64bits_slicer_420_release_package.txt
