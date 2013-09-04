d4230603b7802a95b79213e40110ac8b16eca869
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2013-09-04 06:52:42 -0400

    Add 4.3 release scripts

---
+++ factory-south-win7-vs2008-64bits_make-slicer-430-package.bat
@@ -0,0 +1,3 @@
+
+REM Build Slicer 4.3.0 package using vs2008 64bits
+"C:\D\Support\CMake 2.8.10.2\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicer_430_release_package.cmake" -C Release -V -VV -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicer_430_release_package.txt
