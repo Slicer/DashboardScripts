d9bbac5ffe937aade1dd73be8341dc278a167664
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2013-10-04 05:17:45 -0400

    Add scripts for 4.3.1 patch release

---
+++ factory-south-ubuntu-64bits_make-slicer-431-package.sh
@@ -0,0 +1,9 @@
+export DISPLAY=:0.0 # just DISPLAY=:0.0 without export is not enough
+
+HOSTNAME=factory-south-ubuntu
+DASHBOARD_SCRIPTS_DIR=/home/kitware/Dashboards/DashboardScripts
+LOG_DIR=/home/kitware/Dashboards/Logs
+PACKAGE_VERSION=431
+
+# Package build of slicer
+/home/kitware/Dashboards/Support/cmake-2.8.10.2-Linux-i386/bin/ctest -S $DASHBOARD_SCRIPTS_DIR/${HOSTNAME}-64bits_slicer_${PACKAGE_VERSION}_release_package.cmake -VV -O $LOG_DIR/${HOSTNAME}-64bits_slicer_${PACKAGE_VERSION}_release_package.txt
---
+++ factory-south-win7-vs2008-32bits_make-slicer-431-package.bat
@@ -0,0 +1,4 @@
+
+REM Build Slicer 4.3.1 package using vs2008 32bits
+"C:\D\Support\CMake 2.8.10.2\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-32bits_slicer_431_release_package.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-32bits_slicer_431_release_package.txt
+
