f71580cf91bd7f37fd0e0bba29741453c89c9616
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-04-01 15:40:44 -0400

    Remove obsolete factory-win7* scripts
    factory-south-win7 is now used

--- factory-win7-vs2008-32bits_slicer4_release_continuous.bat
+++
@@ -1,1 +0,0 @@
-"C:\D\Support\cmake-2.8.8-win32-x86\bin\ctest.exe" -S factory-win7-vs2008-32bits_slicer4_release_continuous.cmake -C Release -V -VV -O "C:\D\Logs\factory-win7-vs2008-32bits_slicer4_release_continuous.txt"
--- factory-win7-vs2008-32bits_slicer_debug_experimental.bat
+++
@@ -1,1 +0,0 @@
-"C:\D\Support\cmake-2.8.8-win32-x86\bin\ctest.exe" -S factory-win7-vs2008-32bits_slicer4_debug_experimental.cmake -C Debug -V -VV
