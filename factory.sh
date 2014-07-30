bac768be437879d504acd5aed0b7c0c0d6a9851e
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-07-29 23:07:40 -0400

    Update factory nightly script to use CMake 3.0.0 to build Slicer
    See r23509 [1]
    
    [1] http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=23509

--- factory.sh
+++ factory.sh
@@ -3,19 +3,19 @@ export DISPLAY=:0.0 # just DISPLAY=:0.0 without export is not enough
 export PATH=:/usr/local/git/bin:$PATH
 
 # Nightly build of slicer
-/Users/kitware/Dashboards/Support/CMake\ 2.8-12.app/Contents/bin/ctest -S /Users/kitware/DashboardScripts/factory-64bits_slicer4_release_nightly.cmake -VV -O /Users/kitware/Dashboards/Logs/factory-64bits_slicer4_release_nightly.log
+/Users/kitware/Dashboards/Support/CMake\ 3.0.0.app/Contents/bin/ctest -S /Users/kitware/DashboardScripts/factory-64bits_slicer4_release_nightly.cmake -VV -O /Users/kitware/Dashboards/Logs/factory-64bits_slicer4_release_nightly.log
 
 # Nightly build of slicer extensions testing
-/Users/kitware/Dashboards/Support/CMake\ 2.8-12.app/Contents/bin/ctest -S /Users/kitware/DashboardScripts/factory-64bits_slicerextensions_testing_release_nightly.cmake -VV -O /Users/kitware/Dashboards/Logs/factory-64bits_slicerextensions_testing_release_nightly.log
+/Users/kitware/Dashboards/Support/CMake\ 3.0.0.app/Contents/bin/ctest -S /Users/kitware/DashboardScripts/factory-64bits_slicerextensions_testing_release_nightly.cmake -VV -O /Users/kitware/Dashboards/Logs/factory-64bits_slicerextensions_testing_release_nightly.log
 
 # Nightly build of slicer extensions
-/Users/kitware/Dashboards/Support/CMake\ 2.8-12.app/Contents/bin/ctest -S /Users/kitware/DashboardScripts/factory-64bits_slicerextensions_release_nightly.cmake -VV -O /Users/kitware/Dashboards/Logs/factory-64bits_slicerextensions_release_nightly.log
+/Users/kitware/Dashboards/Support/CMake\ 3.0.0.app/Contents/bin/ctest -S /Users/kitware/DashboardScripts/factory-64bits_slicerextensions_release_nightly.cmake -VV -O /Users/kitware/Dashboards/Logs/factory-64bits_slicerextensions_release_nightly.log
 
 # Nightly build of slicer 4.3 extensions
 /Applications/CMake\ 2.8-10.app/Contents/bin/ctest -S /Users/kitware/DashboardScripts/factory-64bits_slicerextensions_43_release_nightly.cmake -VV -O /Users/kitware/Dashboards/Logs/factory-64bits_slicerextensions_43_release_nightly.log
 
 # Nightly build of CTKAppLauncher
-/Users/kitware/Dashboards/Support/CMake\ 2.8-12.app/Contents/bin/ctest -S /Users/kitware/DashboardScripts/factory-64bits_CTKAppLauncher_release_nightly.cmake -VV -O /Users/kitware/Dashboards/Logs/factory-64bits_CTKAppLauncher_release_nightly.log
+/Users/kitware/Dashboards/Support/CMake\ 3.0.0.app/Contents/bin/ctest -S /Users/kitware/DashboardScripts/factory-64bits_CTKAppLauncher_release_nightly.cmake -VV -O /Users/kitware/Dashboards/Logs/factory-64bits_CTKAppLauncher_release_nightly.log
 
 # Nightly build of Slicer's VTK
 #/Users/kitware/Dashboards/Support/CMake\ 2.8-12.app/Contents/bin/ctest -S /Users/kitware/DashboardScripts/factory_slicer_vtk_shared.cmake -V
