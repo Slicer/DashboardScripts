523f312f19124a0b958f92a1804b73a9d9ba901f
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-07-30 14:15:39 -0400

    Use shorter root path in script building Slicer trunk
    This is an attempt to address the "too much argument" error passed
    to the meta object compiler on Linux.
    
    For example, for the build directory of Slicer:
    
    * Before:
    
      /home/kitware/Dashboards/Nightly/Slicer4
      /home/kitware/Dashboards/Nightly/Slicer-build-64bits-QT4.8.5-PythonQt-With-Tcl-CLI-Release-nightly
    
    * After:
    
      /home/kitware/Dashboards/Nightly/Slicer-0
      /home/kitware/Dashboards/Nightly/Slicer-0-build
      /home/kitware/Dashboards/Nightly/Slicer-0-build - 64bits-QT4.8.5-NoVTKDebugLeaks-Doxygen-Release-Nightly.tx
    
    For sake of consistency, scripts for windows, MacOSx and Linux have
    been updated.

--- factory-south-ubuntu-64bits_slicer4_release_nightly_valgrind.cmake
+++ factory-south-ubuntu-64bits_slicer4_release_nightly_valgrind.cmake
@@ -1,61 +1,139 @@
-cmake_minimum_required(VERSION 2.8.2)
+####################################################################################
+# OS      : GNU/Linux
+# Hardware: x86_64
+# GPU     : OpenGL vendor string: Parallels and ATI Technologies Inc.
+#           OpenGL renderer string: Parallels using ATI Radeon HD 5770 OpenGL Engine
+#           OpenGL version string: 2.1 ATI-1.8.19
+#           OpenGL shading language version string: 1.2
+####################################################################################
+# WARNING - The specific version and processor type of this machine should be reported
+# in the header above. Indeed, this file will be send to the dashboard as a NOTE file.
+#
+# On linux, you could run:
+#     'uname -o' and 'cat /etc/*-release' to obtain the OS name.
+#     'uname -mpi' to obtain hardware details.
+#     'glxinfo | grep OpenGL' to obtain GPU details.
+####################################################################################
 
-include(${CTEST_SCRIPT_DIRECTORY}/CMakeDashboardScriptUtils.cmake)
+cmake_minimum_required(VERSION 2.8.9)
 
 #
+# For additional information, see http://www.slicer.org/slicerWiki/index.php/Documentation/Nightly/Developers/Tutorials/DashboardSetup
+#
+
+#-----------------------------------------------------------------------------
+# Experimental:
+#     - run_ctest() macro will be called *ONE* time
+#     - binary directory will *NOT* be cleaned
+# Continuous:
+#     - run_ctest() macro will be called EVERY 5 minutes ...
+#     - binary directory will *NOT* be cleaned
+#     - configure/build will be executed *ONLY* if the repository has been updated
+# Nightly:
+#     - run_ctest() macro will be called *ONE* time
+#     - binary directory *WILL BE* cleaned
+set(SCRIPT_MODE "Nightly") # "Experimental", "Continuous", "Nightly"
+
+# You could invoke the script with the following syntax:
+#  ctest -S karakoram_Slicer4_nightly.cmake -C <CTEST_BUILD_CONFIGURATION> -V
+#
+# Note that '-C <CTEST_BUILD_CONFIGURATION>' is mandatory on windows
+
+#-----------------------------------------------------------------------------
 # Dashboard properties
-#
+#-----------------------------------------------------------------------------
 set(HOSTNAME              "factory-south-ubuntu")
-set(CTEST_DASHBOARD_ROOT  "$ENV{HOME}/Dashboards/Nightly/")
+set(CTEST_DASHBOARD_ROOT  "$ENV{HOME}/Dashboards/${SCRIPT_MODE}")
 
-set(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE "${CTEST_SCRIPT_DIRECTORY}/${HOSTNAME}-ubuntu_slicer4_valgrind_supression.txt")
-set(CTEST_MEMORYCHECK_COMMAND_OPTIONS "--gen-suppressions=all --trace-children=yes -q --leak-check=yes --show-reachable=yes --num-callers=100")
+set(SVN_BRANCH "trunk")
+set(SVN_REVISION "")
 
-#
+# Each dashboard script should specify a unique ID per CTEST_DASHBOARD_ROOT.
+# It means the following directories will be created:
+#   <CTEST_DASHBOARD_ROOT>/<DIRECTORY_NAME>-<DIRECTORY_IDENTIFIER>        # Source directory
+#   <CTEST_DASHBOARD_ROOT>/<DIRECTORY_NAME>-<DIRECTORY_IDENTIFIER>-build  # Build directory
+set(DIRECTORY_IDENTIFIER  "2")
+
+# Open a shell and type in "cmake --help" to obtain the proper spelling of the generator
+#set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+#set(MY_BITNESS            "64")
+
+#-----------------------------------------------------------------------------
 # Dashboard options
-#
+#-----------------------------------------------------------------------------
 set(WITH_KWSTYLE FALSE)
 set(WITH_MEMCHECK TRUE)
 set(WITH_COVERAGE FALSE)
 set(WITH_DOCUMENTATION FALSE)
 #set(DOCUMENTATION_ARCHIVES_OUTPUT_DIRECTORY ) # for example: $ENV{HOME}/Projects/Doxygen
-set(WITH_PACKAGES OFF)
+set(WITH_PACKAGES FALSE)
 set(CTEST_BUILD_CONFIGURATION "Release")
-set(CTEST_BUILD_FLAGS "-j9 -l8") # Use multiple CPU cores to build. For example "-j4 -l3" on unix
+#set(CTEST_TEST_TIMEOUT 500)
+set(CTEST_BUILD_FLAGS "-j5 -l4") # Use multiple CPU cores to build. For example "-j -l4" on unix
+set(CTEST_PARALLEL_LEVEL 8) # Number of tests running in parallel
 
-set(CTEST_INCLUDED_SCRIPT_NAME ${HOSTNAME}_slicer_common.cmake)
-include(${CTEST_SCRIPT_DIRECTORY}/${CTEST_INCLUDED_SCRIPT_NAME})
-
-set(SCRIPT_MODE "nightly") # "experimental", "continuous", "nightly"
-
-# You could invoke the script with the following syntax:
-#  ctest -S karakoram_Slicer4_nightly.cmake -V
-
-#
+#-----------------------------------------------------------------------------
 # Additional CMakeCache options
-#
+#-----------------------------------------------------------------------------
+set(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE "${CTEST_SCRIPT_DIRECTORY}/${HOSTNAME}-ubuntu_slicer4_valgrind_supression.txt")
+set(CTEST_MEMORYCHECK_COMMAND_OPTIONS "--gen-suppressions=all --trace-children=yes -q --leak-check=yes --show-reachable=yes --num-callers=100")
 set(ADDITIONAL_CMAKECACHE_OPTION "
-  GIT_EXECUTABLE:FILEPATH=/home/kitware/Dashboards/Support/git-1.7.3.4/git
   MEMORYCHECK_COMMAND:FILEPATH=${CTEST_MEMORYCHECK_COMMAND}
   MEMORYCHECK_SUPPRESSIONS_FILE:FILEPATH=${CTEST_MEMORYCHECK_SUPPRESSIONS_FILE}
   MEMORYCHECK_COMMAND_OPTIONS:STRING=${CTEST_MEMORYCHECK_COMMAND_OPTIONS}
+  Slicer_BUILD_CLI:BOOL=ON
+  Slicer_USE_PYTHONQT_WITH_OPENSSL:BOOL=ON
 ")
 
-set(BUILD_OPTIONS_STRING "PythonQt-With-Tcl-CLI-Valgrind") 
-
-#
-# Project specific properties
-#
-set(CTEST_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer4")
-set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer-build-${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE}")
-
+#-----------------------------------------------------------------------------
 # List of test that should be explicitly disabled on this machine
+#-----------------------------------------------------------------------------
 set(TEST_TO_EXCLUDE_REGEX "qMRMLLayoutManagerTest3")
 
-# set any extra environment variables here
+#-----------------------------------------------------------------------------
+# Set any extra environment variables here
+#-----------------------------------------------------------------------------
 if(UNIX)
   set(ENV{DISPLAY} ":0")
 endif()
+
+#-----------------------------------------------------------------------------
+# Required executables
+#-----------------------------------------------------------------------------
+find_program(CTEST_SVN_COMMAND NAMES svn)
+find_program(CTEST_GIT_COMMAND NAMES git)
+find_program(CTEST_COVERAGE_COMMAND NAMES gcov)
+find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
+
+#-----------------------------------------------------------------------------
+# Factory settings
+#-----------------------------------------------------------------------------
+set(CTEST_INCLUDED_SCRIPT_NAME ${HOSTNAME}_slicer_common.cmake)
+include(${CTEST_SCRIPT_DIRECTORY}/${CTEST_INCLUDED_SCRIPT_NAME})
+
+#-----------------------------------------------------------------------------
+# Build Name
+#-----------------------------------------------------------------------------
+# Update the following variable to match the chosen build options. This variable is used to
+# generate both the build directory and the build name.
+# See http://www.cdash.org/CDash/index.php?project=Slicer4 for examples
+set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-Valgrind")
+
+#-----------------------------------------------------------------------------
+# Directory name
+#-----------------------------------------------------------------------------
+set(DIRECTORY_NAME "Slicer")
+
+#-----------------------------------------------------------------------------
+# Build directory
+#-----------------------------------------------------------------------------
+set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/${DIRECTORY_NAME}-${DIRECTORY_IDENTIFIER}-build")
+file(WRITE "${CTEST_DASHBOARD_ROOT}/${DIRECTORY_NAME}-${DIRECTORY_IDENTIFIER}-build - ${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE}.txt" "Generated by ${CTEST_SCRIPT_NAME}")
+
+#-----------------------------------------------------------------------------
+# Source directory
+#-----------------------------------------------------------------------------
+set(CTEST_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/${DIRECTORY_NAME}-${DIRECTORY_IDENTIFIER}")
 
 
 ##########################################
@@ -68,20 +146,38 @@ set(CTEST_NOTES_FILES "${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}")
 # Project specific properties
 #
 set(CTEST_PROJECT_NAME "Slicer4")
-set(CTEST_BUILD_NAME "${MY_OPERATING_SYSTEM}-${MY_COMPILER}-QT${MY_QT_VERSION}-${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}")
+set(CTEST_BUILD_NAME "${MY_OPERATING_SYSTEM}-${MY_COMPILER}-${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}")
 
 #
 # Display build info
 #
-message("site name: ${CTEST_SITE}")
-message("build name: ${CTEST_BUILD_NAME}")
-message("script mode: ${SCRIPT_MODE}")
-message("coverage: ${WITH_COVERAGE}, memcheck: ${WITH_MEMCHECK}")
+message("CTEST_SITE ................: ${CTEST_SITE}")
+message("CTEST_BUILD_NAME ..........: ${CTEST_BUILD_NAME}")
+message("SCRIPT_MODE ...............: ${SCRIPT_MODE}")
+message("CTEST_BUILD_CONFIGURATION .: ${CTEST_BUILD_CONFIGURATION}")
+message("WITH_KWSTYLE ..............: ${WITH_KWSTYLE}")
+message("WITH_COVERAGE: ............: ${WITH_COVERAGE}")
+message("WITH_MEMCHECK .............: ${WITH_MEMCHECK}")
+message("WITH_PACKAGES .............: ${WITH_PACKAGES}")
+message("WITH_DOCUMENTATION ........: ${WITH_DOCUMENTATION}")
+message("DOCUMENTATION_ARCHIVES_OUTPUT_DIRECTORY: ${DOCUMENTATION_ARCHIVES_OUTPUT_DIRECTORY}")
 
 #
-# Download and include dashboard driver script 
+# Convenient function allowing to download a file
 #
-set(url http://svn.slicer.org/Slicer4/trunk/CMake/SlicerDashboardDriverScript.cmake)
+function(download_file url dest)
+  file(DOWNLOAD ${url} ${dest} STATUS status)
+  list(GET status 0 error_code)
+  list(GET status 1 error_msg)
+  if(error_code)
+    message(FATAL_ERROR "error: Failed to download ${url} - ${error_msg}")
+  endif()
+endfunction()
+
+#
+# Download and include dashboard driver script
+#
+set(url http://svn.slicer.org/Slicer4/${SVN_BRANCH}/CMake/SlicerDashboardDriverScript.cmake)
 set(dest ${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}.driver)
 download_file(${url} ${dest})
-INCLUDE(${dest})
+include(${dest})
