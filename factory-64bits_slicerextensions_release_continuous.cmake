Slicer/DashboardScripts

commit 523f312f19124a0b958f92a1804b73a9d9ba901f

Use shorter root path in script building Slicer trunk
This is an attempt to address the "too much argument" error passed
to the meta object compiler on Linux.

For example, for the build directory of Slicer:

* Before:

  /home/kitware/Dashboards/Nightly/Slicer4
  /home/kitware/Dashboards/Nightly/Slicer-build-64bits-QT4.8.5-PythonQt-With-Tcl-CLI-Release-nightly

* After:

  /home/kitware/Dashboards/Nightly/Slicer-0
  /home/kitware/Dashboards/Nightly/Slicer-0-build
  /home/kitware/Dashboards/Nightly/Slicer-0-build - 64bits-QT4.8.5-NoVTKDebugLeaks-Doxygen-Release-Nightly.tx

For sake of consistency, scripts for windows, MacOSx and Linux have
been updated.

factory-64bits_slicerextensions_release_continuous.cmake
@@ -29,8 +29,6 @@ set(SCRIPT_MODE "continuous") # "experimental", "continuous", "nightly"
 # Additional CMakeCache options
 #-----------------------------------------------------------------------------
 set(ADDITIONAL_CMAKECACHE_OPTION "
-  ADDITIONAL_C_FLAGS:STRING=
-  ADDITIONAL_CXX_FLAGS:STRING=
 ")
 
 #-----------------------------------------------------------------------------
@@ -57,21 +55,24 @@ include(${CTEST_SCRIPT_DIRECTORY}/${CTEST_INCLUDED_SCRIPT_NAME})
 #-----------------------------------------------------------------------------
 # Update the following variable to match the chosen build options. This variable is used to
 # generate both the build directory and the build name.
-set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-PythonQt-With-Tcl-CLI")
+set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}")
 
 #-----------------------------------------------------------------------------
 # Build directories
 #-----------------------------------------------------------------------------
 set(dir_suffix ${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION})
+set(dir_identifier "0")
 
-set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/../Nightly/Slicer4-build-${dir_suffix}-nightly/Slicer-build")
+set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/../Nightly/Slicer-${dir_identifier}-build/Slicer-build")
 
 set(testing_suffix "")
 if(EXTENSIONS_BUILDSYSTEM_TESTING)
   set(testing_suffix "-Testing")
 endif()
 
-set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/SlicerExtensions-build-${dir_suffix}-${SCRIPT_MODE}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}")
+#set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/SlicerExtensions-build-${dir_suffix}-${SCRIPT_MODE}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}")
+set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b")
+file(WRITE "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b - SlicerExtensions-build-${dir_suffix}-${SCRIPT_MODE}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}.txt" "Generated by ${CTEST_SCRIPT_NAME}")
 
 #-----------------------------------------------------------------------------
 # Source directories

factory-64bits_slicerextensions_release_nightly.cmake
@@ -29,8 +29,6 @@ set(SCRIPT_MODE "nightly") # "experimental", "continuous", "nightly"
 # Additional CMakeCache options
 #-----------------------------------------------------------------------------
 set(ADDITIONAL_CMAKECACHE_OPTION "
-  ADDITIONAL_C_FLAGS:STRING=
-  ADDITIONAL_CXX_FLAGS:STRING=
 ")
 
 #-----------------------------------------------------------------------------
@@ -57,21 +55,24 @@ include(${CTEST_SCRIPT_DIRECTORY}/${CTEST_INCLUDED_SCRIPT_NAME})
 #-----------------------------------------------------------------------------
 # Update the following variable to match the chosen build options. This variable is used to
 # generate both the build directory and the build name.
-set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-PythonQt-With-Tcl-CLI")
+set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-NoVTKDebugLeaks")
 
 #-----------------------------------------------------------------------------
 # Build directories
 #-----------------------------------------------------------------------------
 set(dir_suffix ${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE})
+set(dir_identifier "0")
 
-set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/Slicer-build-${dir_suffix}/Slicer-build")
+set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/Slicer-${dir_identifier}-build/Slicer-build")
 
 set(testing_suffix "")
 if(EXTENSIONS_BUILDSYSTEM_TESTING)
   set(testing_suffix "-Testing")
 endif()
 
-set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/SlicerExtensions-build-${dir_suffix}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}")
+#set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/SlicerExtensions-build-${dir_suffix}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}")
+set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b")
+file(WRITE "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b - SlicerExtensions-build-${dir_suffix}-${SCRIPT_MODE}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}.txt" "Generated by ${CTEST_SCRIPT_NAME}")
 
 #-----------------------------------------------------------------------------
 # Source directories

factory-64bits_slicerextensions_testing_release_nightly.cmake
@@ -29,8 +29,6 @@ set(SCRIPT_MODE "nightly") # "experimental", "continuous", "nightly"
 # Additional CMakeCache options
 #-----------------------------------------------------------------------------
 set(ADDITIONAL_CMAKECACHE_OPTION "
-  ADDITIONAL_C_FLAGS:STRING=
-  ADDITIONAL_CXX_FLAGS:STRING=
 ")
 
 #-----------------------------------------------------------------------------
@@ -57,21 +55,24 @@ include(${CTEST_SCRIPT_DIRECTORY}/${CTEST_INCLUDED_SCRIPT_NAME})
 #-----------------------------------------------------------------------------
 # Update the following variable to match the chosen build options. This variable is used to
 # generate both the build directory and the build name.
-set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-PythonQt-With-Tcl-CLI")
+set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-NoVTKDebugLeaks")
 
 #-----------------------------------------------------------------------------
 # Build directories
 #-----------------------------------------------------------------------------
 set(dir_suffix ${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE})
+set(dir_identifier "0")
 
-set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/Slicer-build-${dir_suffix}/Slicer-build")
+set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/Slicer-${dir_identifier}-build/Slicer-build")
 
 set(testing_suffix "")
 if(EXTENSIONS_BUILDSYSTEM_TESTING)
   set(testing_suffix "-Testing")
 endif()
 
-set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/SlicerExtensions-build-${dir_suffix}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}")
+#set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/SlicerExtensions-build-${dir_suffix}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}")
+set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b")
+file(WRITE "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b - SlicerExtensions-build-${dir_suffix}-${SCRIPT_MODE}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}.txt" "Generated by ${CTEST_SCRIPT_NAME}")
 
 #-----------------------------------------------------------------------------
 # Source directories

factory-south-ubuntu-64bits_slicerextensions_release_continuous.cmake
@@ -29,8 +29,6 @@ set(SCRIPT_MODE "continuous") # "experimental", "continuous", "nightly"
 # Additional CMakeCache options
 #-----------------------------------------------------------------------------
 set(ADDITIONAL_CMAKECACHE_OPTION "
-  ADDITIONAL_C_FLAGS:STRING=
-  ADDITIONAL_CXX_FLAGS:STRING=
 ")
 
 #-----------------------------------------------------------------------------
@@ -57,21 +55,24 @@ include(${CTEST_SCRIPT_DIRECTORY}/${CTEST_INCLUDED_SCRIPT_NAME})
 #-----------------------------------------------------------------------------
 # Update the following variable to match the chosen build options. This variable is used to
 # generate both the build directory and the build name.
-set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-PythonQt-With-Tcl-CLI")
+set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}")
 
 #-----------------------------------------------------------------------------
 # Build directories
 #-----------------------------------------------------------------------------
 set(dir_suffix ${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION})
+set(dir_identifier "0")
 
-set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/../Nightly/Slicer-build-${dir_suffix}-nightly/Slicer-build")
+set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/../Nightly/Slicer-${dir_identifier}-build/Slicer-build")
 
 set(testing_suffix "")
 if(EXTENSIONS_BUILDSYSTEM_TESTING)
   set(testing_suffix "-Testing")
 endif()
 
-set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/SlicerExtensions-build-${dir_suffix}-${SCRIPT_MODE}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}")
+#set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/SlicerExtensions-build-${dir_suffix}-${SCRIPT_MODE}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}")
+set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b")
+file(WRITE "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b - SlicerExtensions-build-${dir_suffix}-${SCRIPT_MODE}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}.txt" "Generated by ${CTEST_SCRIPT_NAME}")
 
 #-----------------------------------------------------------------------------
 # Source directories

factory-south-ubuntu-64bits_slicerextensions_release_nightly.cmake
@@ -29,8 +29,6 @@ set(SCRIPT_MODE "nightly") # "experimental", "continuous", "nightly"
 # Additional CMakeCache options
 #-----------------------------------------------------------------------------
 set(ADDITIONAL_CMAKECACHE_OPTION "
-  ADDITIONAL_C_FLAGS:STRING=
-  ADDITIONAL_CXX_FLAGS:STRING=
 ")
 
 #-----------------------------------------------------------------------------
@@ -57,21 +55,24 @@ include(${CTEST_SCRIPT_DIRECTORY}/${CTEST_INCLUDED_SCRIPT_NAME})
 #-----------------------------------------------------------------------------
 # Update the following variable to match the chosen build options. This variable is used to
 # generate both the build directory and the build name.
-set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-PythonQt-With-Tcl-CLI")
+set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-NoVTKDebugLeaks")
 
 #-----------------------------------------------------------------------------
 # Build directories
 #-----------------------------------------------------------------------------
 set(dir_suffix ${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE})
+set(dir_identifier "0")
 
-set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/Slicer-build-${dir_suffix}/Slicer-build")
+set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/Slicer-${dir_identifier}-build/Slicer-build")
 
 set(testing_suffix "")
 if(EXTENSIONS_BUILDSYSTEM_TESTING)
   set(testing_suffix "-Testing")
 endif()
 
-set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/SlicerExtensions-build-${dir_suffix}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}")
+#set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/SlicerExtensions-build-${dir_suffix}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}")
+set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b")
+file(WRITE "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b - SlicerExtensions-build-${dir_suffix}-${SCRIPT_MODE}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}.txt" "Generated by ${CTEST_SCRIPT_NAME}")
 
 #-----------------------------------------------------------------------------
 # Source directories

factory-south-ubuntu-64bits_slicerextensions_testing_release_nightly.cmake
@@ -29,8 +29,6 @@ set(SCRIPT_MODE "nightly") # "experimental", "continuous", "nightly"
 # Additional CMakeCache options
 #-----------------------------------------------------------------------------
 set(ADDITIONAL_CMAKECACHE_OPTION "
-  ADDITIONAL_C_FLAGS:STRING=
-  ADDITIONAL_CXX_FLAGS:STRING=
 ")
 
 #-----------------------------------------------------------------------------
@@ -57,21 +55,24 @@ include(${CTEST_SCRIPT_DIRECTORY}/${CTEST_INCLUDED_SCRIPT_NAME})
 #-----------------------------------------------------------------------------
 # Update the following variable to match the chosen build options. This variable is used to
 # generate both the build directory and the build name.
-set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-PythonQt-With-Tcl-CLI")
+set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-NoVTKDebugLeaks")
 
 #-----------------------------------------------------------------------------
 # Build directories
 #-----------------------------------------------------------------------------
 set(dir_suffix ${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE})
+set(dir_identifier "0")
 
-set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/Slicer-build-${dir_suffix}/Slicer-build")
+set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/Slicer-${dir_identifier}-build/Slicer-build")
 
 set(testing_suffix "")
 if(EXTENSIONS_BUILDSYSTEM_TESTING)
   set(testing_suffix "-Testing")
 endif()
 
-set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/SlicerExtensions-build-${dir_suffix}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}")
+#set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/SlicerExtensions-build-${dir_suffix}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}")
+set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b")
+file(WRITE "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b - SlicerExtensions-build-${dir_suffix}-${SCRIPT_MODE}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}.txt" "Generated by ${CTEST_SCRIPT_NAME}")
 
 #-----------------------------------------------------------------------------
 # Source directories

factory-south-win7-vs2008-64bits_slicerextensions_release_continuous.cmake
@@ -31,8 +31,6 @@ set(SCRIPT_MODE "continuous") # "experimental", "continuous", "nightly"
 # Additional CMakeCache options
 #-----------------------------------------------------------------------------
 set(ADDITIONAL_CMAKECACHE_OPTION "
-  ADDITIONAL_C_FLAGS:STRING=
-  ADDITIONAL_CXX_FLAGS:STRING=
 ")
 
 #-----------------------------------------------------------------------------
@@ -59,15 +57,15 @@ include(${CTEST_SCRIPT_DIRECTORY}/${CTEST_INCLUDED_SCRIPT_NAME})
 #-----------------------------------------------------------------------------
 # Update the following variable to match the chosen build options. This variable is used to
 # generate both the build directory and the build name.
-set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-PythonQt-With-Tcl-CLI")
+set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}s")
 
 #-----------------------------------------------------------------------------
 # Build directories
 #-----------------------------------------------------------------------------
 set(dir_suffix ${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION})
+set(dir_identifier "1")
 
-#set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/../N/Slicer-build-${dir_suffix}-nightly/Slicer-build")
-set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/../N/Slicer-1-build/Slicer-build")
+set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/../N/Slicer-${dir_identifier}-build/Slicer-build")
 
 set(testing_suffix "")
 if(EXTENSIONS_BUILDSYSTEM_TESTING)
@@ -75,8 +73,8 @@ if(EXTENSIONS_BUILDSYSTEM_TESTING)
 endif()
 
 #set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/SlicerExtensions-build-${dir_suffix}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}")
-set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/S-1-E${testing_suffix}-b")
-file(WRITE "${CTEST_DASHBOARD_ROOT}/S-1-E${testing_suffix}-b - SlicerExtensions-build-${dir_suffix}-${SCRIPT_MODE}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}.txt" "Generated by ${CTEST_SCRIPT_NAME}")
+set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b")
+file(WRITE "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b - SlicerExtensions-build-${dir_suffix}-${SCRIPT_MODE}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}.txt" "Generated by ${CTEST_SCRIPT_NAME}")
 
 #-----------------------------------------------------------------------------
 # Source directories
@@ -95,7 +93,7 @@ set(CTEST_NOTES_FILES "${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}")
 set(CTEST_PROJECT_NAME "Slicer4")
 set(CTEST_BUILD_NAME "${MY_OPERATING_SYSTEM}-${MY_COMPILER}-${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}")
 
- 
+
 
 #
 # Download and include dashboard driver script

factory-south-win7-vs2008-64bits_slicerextensions_release_nightly.cmake
@@ -31,8 +31,6 @@ set(SCRIPT_MODE "nightly") # "experimental", "continuous", "nightly"
 # Additional CMakeCache options
 #-----------------------------------------------------------------------------
 set(ADDITIONAL_CMAKECACHE_OPTION "
-  ADDITIONAL_C_FLAGS:STRING=
-  ADDITIONAL_CXX_FLAGS:STRING=
 ")
 
 #-----------------------------------------------------------------------------
@@ -59,15 +57,15 @@ include(${CTEST_SCRIPT_DIRECTORY}/${CTEST_INCLUDED_SCRIPT_NAME})
 #-----------------------------------------------------------------------------
 # Update the following variable to match the chosen build options. This variable is used to
 # generate both the build directory and the build name.
-set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-PythonQt-With-Tcl-CLI")
+set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-NoVTKDebugLeaks-NoConsole")
 
 #-----------------------------------------------------------------------------
 # Build directories
 #-----------------------------------------------------------------------------
 set(dir_suffix ${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE})
+set(dir_identifier "1")
 
-#set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/Slicer-build-${dir_suffix}/Slicer-build")
-set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/Slicer-1-build/Slicer-build")
+set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/Slicer-${dir_identifier}-build/Slicer-build")
 
 set(testing_suffix "")
 if(EXTENSIONS_BUILDSYSTEM_TESTING)
@@ -75,8 +73,8 @@ if(EXTENSIONS_BUILDSYSTEM_TESTING)
 endif()
 
 #set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/SlicerExtensions-build-${dir_suffix}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}")
-set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/S-1-E${testing_suffix}-b")
-file(WRITE "${CTEST_DASHBOARD_ROOT}/S-1-E${testing_suffix}-b - SlicerExtensions-build-${dir_suffix}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}.txt" "Generated by ${CTEST_SCRIPT_NAME}")
+set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b")
+file(WRITE "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b - SlicerExtensions-build-${dir_suffix}-${SCRIPT_MODE}${testing_suffix}-${EXTENSIONS_TRACK_QUALIFIER}.txt" "Generated by ${CTEST_SCRIPT_NAME}")
 
 #-----------------------------------------------------------------------------
 # Source directories
@@ -95,7 +93,7 @@ set(CTEST_NOTES_FILES "${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}")
 set(CTEST_PROJECT_NAME "Slicer4")
 set(CTEST_BUILD_NAME "${MY_OPERATING_SYSTEM}-${MY_COMPILER}-${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}")
 
- 
+
 
 #
 # Download and include dashboard driver script

commit dead89e4c465d681d46d7278dbf27f08a967ffe7

Consistently reference Slicer_DIR and EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY
This commit fixes a regression introduced by 523f312f

factory-64bits_slicerextensions_release_continuous.cmake
@@ -77,7 +77,7 @@ file(WRITE "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b - S
 #-----------------------------------------------------------------------------
 # Source directories
 #-----------------------------------------------------------------------------
-set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer4/Extensions/CMake")
+set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/../Nightly/Slicer-${dir_identifier}/Extensions/CMake")
 
 ##########################################
 # WARNING: DO NOT EDIT BEYOND THIS POINT #

factory-64bits_slicerextensions_release_nightly.cmake
@@ -63,7 +63,7 @@ set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-NoVTKDebugLeaks")
 set(dir_suffix ${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE})
 set(dir_identifier "0")
 
-set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/Slicer-${dir_identifier}-build/Slicer-build")
+set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/../Nightly/Slicer-${dir_identifier}-build/Slicer-build")
 
 set(testing_suffix "")
 if(EXTENSIONS_BUILDSYSTEM_TESTING)
@@ -77,7 +77,7 @@ file(WRITE "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b - S
 #-----------------------------------------------------------------------------
 # Source directories
 #-----------------------------------------------------------------------------
-set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer4/Extensions/CMake")
+set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/../Nightly/Slicer-${dir_identifier}/Extensions/CMake")
 
 
 ##########################################

factory-64bits_slicerextensions_testing_release_nightly.cmake
@@ -63,7 +63,7 @@ set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-NoVTKDebugLeaks")
 set(dir_suffix ${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE})
 set(dir_identifier "0")
 
-set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/Slicer-${dir_identifier}-build/Slicer-build")
+set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/../Nightly/Slicer-${dir_identifier}-build/Slicer-build")
 
 set(testing_suffix "")
 if(EXTENSIONS_BUILDSYSTEM_TESTING)
@@ -77,7 +77,7 @@ file(WRITE "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b - S
 #-----------------------------------------------------------------------------
 # Source directories
 #-----------------------------------------------------------------------------
-set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer4/Extensions/CMake")
+set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/../Nightly/Slicer-${dir_identifier}/Extensions/CMake")
 
 
 ##########################################

factory-south-ubuntu-64bits_slicerextensions_release_continuous.cmake
@@ -77,7 +77,7 @@ file(WRITE "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b - S
 #-----------------------------------------------------------------------------
 # Source directories
 #-----------------------------------------------------------------------------
-set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer4-0/Extensions/CMake")
+set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/../Nightly/Slicer-0/Extensions/CMake")
 
 ##########################################
 # WARNING: DO NOT EDIT BEYOND THIS POINT #

factory-south-ubuntu-64bits_slicerextensions_release_nightly.cmake
@@ -63,7 +63,7 @@ set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-NoVTKDebugLeaks")
 set(dir_suffix ${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE})
 set(dir_identifier "0")
 
-set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/Slicer-${dir_identifier}-build/Slicer-build")
+set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/../Nightly/Slicer-${dir_identifier}-build/Slicer-build")
 
 set(testing_suffix "")
 if(EXTENSIONS_BUILDSYSTEM_TESTING)
@@ -77,7 +77,7 @@ file(WRITE "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b - S
 #-----------------------------------------------------------------------------
 # Source directories
 #-----------------------------------------------------------------------------
-set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer4/Extensions/CMake")
+set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/../Nightly/Slicer-${dir_identifier}/Extensions/CMake")
 
 ##########################################
 # WARNING: DO NOT EDIT BEYOND THIS POINT #

factory-south-ubuntu-64bits_slicerextensions_testing_release_nightly.cmake
@@ -63,7 +63,7 @@ set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-NoVTKDebugLeaks")
 set(dir_suffix ${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE})
 set(dir_identifier "0")
 
-set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/Slicer-${dir_identifier}-build/Slicer-build")
+set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/../Nightly/Slicer-${dir_identifier}-build/Slicer-build")
 
 set(testing_suffix "")
 if(EXTENSIONS_BUILDSYSTEM_TESTING)
@@ -77,7 +77,7 @@ file(WRITE "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b - S
 #-----------------------------------------------------------------------------
 # Source directories
 #-----------------------------------------------------------------------------
-set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer4/Extensions/CMake")
+set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/../Nightly/Slicer-${dir_identifier}/Extensions/CMake")
 
 ##########################################
 # WARNING: DO NOT EDIT BEYOND THIS POINT #

factory-south-win7-vs2008-64bits_slicerextensions_release_continuous.cmake
@@ -79,7 +79,7 @@ file(WRITE "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b - S
 #-----------------------------------------------------------------------------
 # Source directories
 #-----------------------------------------------------------------------------
-set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer-1/Extensions/CMake")
+set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/../N/Slicer-${dir_identifier}/Extensions/CMake")
 
 ##########################################
 # WARNING: DO NOT EDIT BEYOND THIS POINT #

factory-south-win7-vs2008-64bits_slicerextensions_release_nightly.cmake
@@ -65,7 +65,7 @@ set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-NoVTKDebugLeaks-N
 set(dir_suffix ${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE})
 set(dir_identifier "1")
 
-set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/Slicer-${dir_identifier}-build/Slicer-build")
+set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/../N/Slicer-${dir_identifier}-build/Slicer-build")
 
 set(testing_suffix "")
 if(EXTENSIONS_BUILDSYSTEM_TESTING)
@@ -79,7 +79,7 @@ file(WRITE "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b - S
 #-----------------------------------------------------------------------------
 # Source directories
 #-----------------------------------------------------------------------------
-set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer-1/Extensions/CMake")
+set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/../N/Slicer-${dir_identifier}/Extensions/CMake")
 
 ##########################################
 # WARNING: DO NOT EDIT BEYOND THIS POINT #

factory-south-win7-vs2008-64bits_slicerextensions_testing_release_nightly.cmake
@@ -65,7 +65,7 @@ set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}-NoVTKDebugLeaks-N
 set(dir_suffix ${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE})
 set(dir_identifier "1")
 
-set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/Slicer-${dir_identifier}-build/Slicer-build")
+set(Slicer_DIR "${CTEST_DASHBOARD_ROOT}/../N/Slicer-${dir_identifier}-build/Slicer-build")
 
 set(testing_suffix "")
 if(EXTENSIONS_BUILDSYSTEM_TESTING)
@@ -79,7 +79,7 @@ file(WRITE "${CTEST_DASHBOARD_ROOT}/S-${dir_identifier}-E${testing_suffix}-b - S
 #-----------------------------------------------------------------------------
 # Source directories
 #-----------------------------------------------------------------------------
-set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer-1/Extensions/CMake")
+set(EXTENSIONS_BUILDSYSTEM_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/../N/Slicer-${dir_identifier}/Extensions/CMake")
 
 ##########################################
 # WARNING: DO NOT EDIT BEYOND THIS POINT #