438b5a7e8e851d25497a24159f600420947600aa
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-11-05 04:28:10 -0500

    Remove 32-bit unused code from windows nightly script

--- factory-south-win7.bat
+++ factory-south-win7.bat
@@ -12,29 +12,8 @@ REM Nightly build of slicer extensions vs2008 64bits
 REM Nightly build of slicer 4.4 extensions vs2008 64bits
 "C:\D\Support\CMake 3.0.1\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicerextensions_44_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicerextensions_44_release_nightly.txt
 
-REM Nightly build of slicer vs2008 32bits
-REM "C:\D\Support\CMake 3.0.1\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-32bits_slicer4_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-32bits_slicer4_release_nightly.txt
-
-REM Nightly build of slicer extensions testing vs2008 32bits
-REM "C:\D\Support\CMake 3.0.1\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-32bits_slicerextensions_testing_release_nightly.cmake" -C Release -VV -O C:\D\Logs\factory-south-win7-vs2008-32bits_slicerextensions_testing_release_nightly.txt
-
-REM Nightly build of slicer extensions vs2008 32bits
-REM "C:\D\Support\CMake 3.0.1\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-32bits_slicerextensions_release_nightly.cmake" -C Release -VV -O C:\D\Logs\factory-south-win7-vs2008-32bits_slicerextensions_release_nightly.txt
-
-REM Nightly build of slicer 4.1.1 extensions vs2008 32bits
-REM "C:\D\Support\CMake 3.0.1\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-32bits_slicerextensions_411_release_nightly.cmake" -C Release -VV -O C:\D\Logs\factory-south-win7-vs2008-32bits_slicerextensions_411_release_nightly.txt
-
-REM Nightly build of slicer 4.3 extensions vs2008 32bits
-REM "C:\D\Support\CMake 3.0.1\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-32bits_slicerextensions_43_release_nightly.cmake" -C Release -VV -O C:\D\Logs\factory-south-win7-vs2008-32bits_slicerextensions_43_release_nightly.txt
-
 REM Nightly build of slicer vs2010 64bits
 REM "C:\D\Support\CMake 3.0.1\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2010-64bits_slicer4_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2010-64bits_slicer4_release_nightly.txt
-
-REM Nightly build of slicer vs2010 32bits
-REM "C:\D\Support\CMake 3.0.1\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2010-32bits_slicer4_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2010-32bits_slicer4_release_nightly.txt
-
-REM Nightly build of CTKAppLauncher vs2008 32bits
-REM "C:\D\Support\CMake 3.0.1\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-32bits_CTKAppLauncher_release_nightly.cmake" -C Release -VV -O C:\D\Logs\factory-south-win7-vs2008-32bits_CTKAppLauncher_release_nightly.txt
 
 REM cd "C:\Dashboards\Client"
 
