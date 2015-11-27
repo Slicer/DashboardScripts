2e956e931d99c15b9239fa3868e7760cb5e94697
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-11-27 16:24:20 -0500

    Update windows factory script to build 4.5 extension first only on weekend
    This commit update the windows nightly script to check the day of the week
    and switch the order of "4.5" and "Nightly" extensions build accordingly.

--- factory-south-win7.bat
+++ factory-south-win7.bat
@@ -1,16 +1,35 @@
-
+@ECHO OFF
 
 REM Nightly build of slicer vs2008 64bits
+::echo "Nightly build of slicer vs2008 64bits"
 "C:\D\Support\CMake 3.1.0-rc3\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicer4_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicer4_release_nightly.txt
 
 REM Nightly build of slicer extensions testing vs2008 64bits
+::echo "Nightly build of slicer extensions testing vs2008 64bits"
 "C:\D\Support\CMake 3.1.0-rc3\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicerextensions_testing_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicerextensions_testing_release_nightly.txt
 
-REM Nightly build of slicer extensions vs2008 64bits
-"C:\D\Support\CMake 3.1.0-rc3\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicerextensions_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicerextensions_release_nightly.txt
+:: See http://serverfault.com/questions/94824/finding-day-of-week-in-batch-file-windows-server-2008
+:: and http://stackoverflow.com/a/14882478/1539918
+for /f "skip=2 tokens=2 delims=," %%a in ('wmic path win32_localtime get dayofweek /format:csv') do set DAYOFWEEK=%%a
 
-REM Nightly build of slicer 4.5 extensions vs2008 64bits
-"C:\D\Support\CMake 3.1.0-rc3\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicerextensions_45_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicerextensions_45_release_nightly.txt
+:: Since nightly windows build starts after midnight, the following weekday are associated with weekend build:
+::  6 -> Saturday
+::  7 -> Sunday
+echo DAYOFWEEK[%DAYOFWEEK%]
+
+set STABLE_BEFORE_NIGHTLY=0
+if "%DAYOFWEEK%"=="6" set STABLE_BEFORE_NIGHTLY=1
+if "%DAYOFWEEK%"=="7" set STABLE_BEFORE_NIGHTLY=1
+
+echo STABLE_BEFORE_NIGHTLY[%STABLE_BEFORE_NIGHTLY%]
+
+if "%STABLE_BEFORE_NIGHTLY%"=="1" (
+  CALL :factory-south-win7-vs2008-64bits_slicerextensions_45_release_nightly
+  CALL :factory-south-win7-vs2008-64bits_slicerextensions_release_nightly
+) else (
+  CALL :factory-south-win7-vs2008-64bits_slicerextensions_release_nightly
+  CALL :factory-south-win7-vs2008-64bits_slicerextensions_45_release_nightly
+)
 
 :: Publish Slicer extension module metadata
 ::call C:\D\DashboardScripts\factory-south-win7-slicer-publish-extension-module-metadata.bat >C:\D\Logs\factory-south-win7-slicer-publish-extension-module-metadata.log 2>&1
@@ -21,3 +40,21 @@ REM "C:\D\Support\CMake 3.1.0-rc3\bin\ctest.exe" -S "C:\D\DashboardScripts\facto
 REM cd "C:\Dashboards\Client"
 
 REM "C:\D\Support\CMake 3.1.0-rc3\bin\ctest.exe" -S "C:\Dashboards\Client\factory.kitware.ctest" -V -O "C:\Dashboards\Logs\itk_cdash_at_home.txt"
+
+:: force execution to quit at the end of the "main" logic
+EXIT /B %ERRORLEVEL%
+
+:: a function to run Nightly extension dashboard
+:factory-south-win7-vs2008-64bits_slicerextensions_release_nightly
+REM Nightly build of slicer extensions vs2008 64bits
+::echo "Nightly build of slicer extensions vs2008 64bits"
+"C:\D\Support\CMake 3.1.0-rc3\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicerextensions_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicerextensions_release_nightly.txt
+EXIT /B 0
+
+
+:: a function to run Release extension dashboard
+:factory-south-win7-vs2008-64bits_slicerextensions_45_release_nightly
+REM Nightly build of slicer 4.5 extensions vs2008 64bits
+::echo "Nightly build of slicer 4.5 extensions vs2008 64bits"
+"C:\D\Support\CMake 3.1.0-rc3\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicerextensions_45_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicerextensions_45_release_nightly.txt
+EXIT /B 0
