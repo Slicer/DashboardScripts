22b531d262b777ee9d3972fd6ca9088f4a67774d
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-01-26 16:59:28 -0500

    factory-south-win7: Switch to CMake 3.4.1 - Part #2

--- factory-south-win7.bat
+++ factory-south-win7.bat
@@ -2,11 +2,11 @@
 
 REM Nightly build of slicer vs2008 64bits
 ::echo "Nightly build of slicer vs2008 64bits"
-"C:\D\Support\CMake 3.1.0-rc3\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicer4_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicer4_release_nightly.txt
+"C:\D\Support\CMake 3.4.1\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicer4_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicer4_release_nightly.txt
 
 REM Nightly build of slicer extensions testing vs2008 64bits
 ::echo "Nightly build of slicer extensions testing vs2008 64bits"
-"C:\D\Support\CMake 3.1.0-rc3\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicerextensions_testing_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicerextensions_testing_release_nightly.txt
+"C:\D\Support\CMake 3.4.1\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicerextensions_testing_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicerextensions_testing_release_nightly.txt
 
 :: See http://serverfault.com/questions/94824/finding-day-of-week-in-batch-file-windows-server-2008
 :: and http://stackoverflow.com/a/14882478/1539918
@@ -35,11 +35,11 @@ if "%STABLE_BEFORE_NIGHTLY%"=="1" (
 ::call C:\D\DashboardScripts\factory-south-win7-slicer-publish-extension-module-metadata.bat >C:\D\Logs\factory-south-win7-slicer-publish-extension-module-metadata.log 2>&1
 
 REM Nightly build of slicer vs2010 64bits
-REM "C:\D\Support\CMake 3.1.0-rc3\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2010-64bits_slicer4_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2010-64bits_slicer4_release_nightly.txt
+REM "C:\D\Support\CMake 3.4.1\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2010-64bits_slicer4_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2010-64bits_slicer4_release_nightly.txt
 
 REM cd "C:\Dashboards\Client"
 
-REM "C:\D\Support\CMake 3.1.0-rc3\bin\ctest.exe" -S "C:\Dashboards\Client\factory.kitware.ctest" -V -O "C:\Dashboards\Logs\itk_cdash_at_home.txt"
+REM "C:\D\Support\CMake 3.4.1\bin\ctest.exe" -S "C:\Dashboards\Client\factory.kitware.ctest" -V -O "C:\Dashboards\Logs\itk_cdash_at_home.txt"
 
 :: force execution to quit at the end of the "main" logic
 EXIT /B %ERRORLEVEL%
@@ -48,7 +48,7 @@ EXIT /B %ERRORLEVEL%
 :factory-south-win7-vs2008-64bits_slicerextensions_release_nightly
 REM Nightly build of slicer extensions vs2008 64bits
 ::echo "Nightly build of slicer extensions vs2008 64bits"
-"C:\D\Support\CMake 3.1.0-rc3\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicerextensions_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicerextensions_release_nightly.txt
+"C:\D\Support\CMake 3.4.1\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicerextensions_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicerextensions_release_nightly.txt
 EXIT /B 0
 
 
@@ -56,5 +56,5 @@ EXIT /B 0
 :factory-south-win7-vs2008-64bits_slicerextensions_45_release_nightly
 REM Nightly build of slicer 4.5 extensions vs2008 64bits
 ::echo "Nightly build of slicer 4.5 extensions vs2008 64bits"
-"C:\D\Support\CMake 3.1.0-rc3\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicerextensions_45_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicerextensions_45_release_nightly.txt
+"C:\D\Support\CMake 3.4.1\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicerextensions_45_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicerextensions_45_release_nightly.txt
 EXIT /B 0
