3172e8b52198dec1c2ca2c41ae011e7f9e5df2b1
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-01-26 17:03:17 -0500

    factory-south-win7: Increase verbosity of nightly build

--- factory-south-win7.bat
+++ factory-south-win7.bat
@@ -2,7 +2,7 @@
 
 REM Nightly build of slicer vs2008 64bits
 ::echo "Nightly build of slicer vs2008 64bits"
-"C:\D\Support\CMake 3.4.1\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicer4_release_nightly.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicer4_release_nightly.txt
+"C:\D\Support\CMake 3.4.1\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicer4_release_nightly.cmake" -C Release -VV -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicer4_release_nightly.txt
 
 REM Nightly build of slicer extensions testing vs2008 64bits
 ::echo "Nightly build of slicer extensions testing vs2008 64bits"
