Slicer/DashboardScripts

commit 549b599eb674c9d24e9c1655873c75d4d536e5bc

Add "factory-south-win7-vs2008-64bits" scripts

factory-south-win7_common.cmake
@@ -1,0 +1,28 @@
+
+message("-- Including ${CTEST_INCLUDED_SCRIPT_NAME}")
+
+set(MY_OPERATING_SYSTEM   "Windows7") # Windows, Linux, Darwin... 
+set(CTEST_SITE            "${HOSTNAME}.kitware") # for example: mymachine.kitware, mymachine.bwh.harvard.edu, ...
+
+get_filename_component(CTEST_SCRIPT_NAME_WE ${CTEST_SCRIPT_NAME} NAME_WE)
+set(CTEST_LOG_FILE        "C:/D/Logs/${CTEST_SCRIPT_NAME_WE}.txt")
+
+set(CTEST_BUILD_FLAGS "")
+
+#find_program(CTEST_GIT_COMMAND NAMES git)
+set(CTEST_GIT_COMMAND "C:/Program Files (x86)/Git/bin/git.exe")
+find_program(CTEST_SVN_COMMAND NAMES svn)
+find_program(CTEST_COVERAGE_COMMAND NAMES gcov)
+find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
+
+#-----------------------------------------------------------------------------
+# Display variables
+#-----------------------------------------------------------------------------
+foreach(var MY_OPERATING_SYSTEM CTEST_SITE
+            CTEST_LOG_FILE CTEST_BUILD_FLAGS
+            CTEST_GIT_COMMAND CTEST_SVN_COMMAND
+            CTEST_COVERAGE_COMMAND CTEST_MEMORYCHECK_COMMAND
+            )
+  message("-- ${var}: ${${var}}")
+endforeach()
+

factory-south-win7-vs2008-64bits_make-slicer-411-package.bat
@@ -1,0 +1,3 @@
+
+REM Build Slicer 4.1.1 package using vs2008 64bits
+"C:\D\Support\CMake 2.8.10.2\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicer_411_release_package.cmake" -C Release -V -VV -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicer_411_release_package.txt

factory-south-win7-vs2008-64bits_make-slicer-420-package.bat
@@ -1,0 +1,3 @@
+
+REM Build Slicer 4.1.1 package using vs2008 64bits
+"C:\D\Support\CMake 2.8.10.2\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicer_420_release_package.cmake" -C Release -V -VV -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicer_420_release_package.txt

factory-south-win7-vs2008-64bits_make-slicer-421-package.bat
@@ -1,0 +1,3 @@
+
+REM Build Slicer 4.2.1 package using vs2008 64bits
+"C:\D\Support\CMake 2.8.10.2\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicer_421_release_package.cmake" -C Release -V -VV -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicer_421_release_package.txt

factory-south-win7-vs2008-64bits_make-slicer-422-package.bat
@@ -1,0 +1,3 @@
+
+REM Build Slicer 4.2.1 package using vs2008 64bits
+"C:\D\Support\CMake 2.8.10.2\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicer_422_release_package.cmake" -C Release -V -VV -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicer_422_release_package.txt

factory-south-win7-vs2008-64bits_make-slicer-package.bat
@@ -1,0 +1,3 @@
+
+REM Build Slicer package using vs2008 64bits
+"C:\D\Support\CMake 2.8.10.2\bin\ctest.exe" -S "C:\D\DashboardScripts\factory-south-win7-vs2008-64bits_slicer4_release_package.cmake" -C Release -V -O C:\D\Logs\factory-south-win7-vs2008-64bits_slicer4_release_package.txt

factory-south-win7-vs2008-64bits_slicer4-itkv4_release_experimental.cmake
@@ -1,0 +1,85 @@
+cmake_minimum_required(VERSION 2.8.2)
+
+include(${CTEST_SCRIPT_DIRECTORY}/CMakeDashboardScriptUtils.cmake)
+
+#
+# Dashboard properties
+#
+set(HOSTNAME              "factory-south-win7")
+set(MY_COMPILER           "VS2008")
+set(CTEST_DASHBOARD_ROOT  "C:/D/E")
+# Open a shell and type in "cmake --help" to obtain the proper spelling of the generator
+set(CTEST_CMAKE_GENERATOR "Visual Studio 9 2008 Win64")
+set(MY_BITNESS            "64")
+
+#
+# Dashboard options
+#
+set(WITH_KWSTYLE FALSE)
+set(WITH_MEMCHECK FALSE)
+set(WITH_COVERAGE FALSE)
+set(WITH_DOCUMENTATION FALSE)
+#set(DOCUMENTATION_ARCHIVES_OUTPUT_DIRECTORY ) # for example: $ENV{HOME}/Projects/Doxygen
+set(WITH_PACKAGES FALSE)
+set(WITH_EXTENSIONS FALSE) # Indicates if 'trusted' Slicer extensions should be
+                          # built, tested, packaged and uploaded.
+set(CTEST_BUILD_CONFIGURATION "Release")
+set(CTEST_TRACK_SUFFIX "-ITKv4")
+
+set(CTEST_INCLUDED_SCRIPT_NAME ${HOSTNAME}_slicer_common.cmake)
+include(${CTEST_SCRIPT_DIRECTORY}/${CTEST_INCLUDED_SCRIPT_NAME})
+
+set(SCRIPT_MODE "experimental") # "experimental", "continuous", "nightly"
+
+# Additional CMakeCache options
+# For example:
+#   ADDITIONAL_{C/CXX}_FLAGS: /MP -> Multi process build with MSVC
+#
+set(ADDITIONAL_CMAKECACHE_OPTION "
+  ADDITIONAL_C_FLAGS:STRING=/MP
+  ADDITIONAL_CXX_FLAGS:STRING=/MP
+  Slicer_USE_PYTHONQT:BOOL=ON
+  Slicer_USE_PYTHONQT_WITH_TCL:BOOL=ON
+  Slicer_BUILD_CLI:BOOL=ON
+  ITK_VERSION_MAJOR:STRING=4
+")
+set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}${CTEST_TRACK_SUFFIX}-PythonQt-With-Tcl-CLI")
+#
+# Project specific properties
+#
+set(CTEST_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer4")
+#set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer-build-${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE}")
+set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer-build-5")
+
+file(WRITE "${CTEST_DASHBOARD_ROOT}/Slicer-build-5 - Slicer-build-${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE}.txt" "Generated by ${CTEST_SCRIPT_NAME}")
+
+# List of test that should be explicitly disabled on this machine
+set(TEST_TO_EXCLUDE_REGEX "qMRMLLayoutManagerTest3")
+# set any extra environment variables here
+if(UNIX)
+  set(ENV{DISPLAY} ":0")
+endif()
+##########################################
+# WARNING: DO NOT EDIT BEYOND THIS POINT #
+##########################################
+set(CTEST_NOTES_FILES "${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}")
+#
+# Project specific properties
+#
+set(CTEST_PROJECT_NAME "Slicer4")
+set(CTEST_BUILD_NAME "${MY_OPERATING_SYSTEM}-${MY_COMPILER}-${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}")
+#
+# Display build info
+#
+message("site name: ${CTEST_SITE}")
+message("build name: ${CTEST_BUILD_NAME}")
+message("script mode: ${SCRIPT_MODE}")
+message("coverage: ${WITH_COVERAGE}, memcheck: ${WITH_MEMCHECK}")
+ 
+#
+# Download and include dashboard driver script 
+#
+set(url http://svn.slicer.org/Slicer4/trunk/CMake/SlicerDashboardDriverScript.cmake)
+set(dest ${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}.driver)
+download_file(${url} ${dest})
+INCLUDE(${dest})

factory-south-win7-vs2008-64bits_slicer4-itkv4_release_nightly.cmake
@@ -1,0 +1,85 @@
+cmake_minimum_required(VERSION 2.8.2)
+
+include(${CTEST_SCRIPT_DIRECTORY}/CMakeDashboardScriptUtils.cmake)
+
+#
+# Dashboard properties
+#
+set(HOSTNAME              "factory-south-win7")
+set(MY_COMPILER           "VS2008")
+set(CTEST_DASHBOARD_ROOT  "C:/D/N")
+# Open a shell and type in "cmake --help" to obtain the proper spelling of the generator
+set(CTEST_CMAKE_GENERATOR "Visual Studio 9 2008 Win64")
+set(MY_BITNESS            "64")
+
+#
+# Dashboard options
+#
+set(WITH_KWSTYLE FALSE)
+set(WITH_MEMCHECK FALSE)
+set(WITH_COVERAGE FALSE)
+set(WITH_DOCUMENTATION FALSE)
+#set(DOCUMENTATION_ARCHIVES_OUTPUT_DIRECTORY ) # for example: $ENV{HOME}/Projects/Doxygen
+set(WITH_PACKAGES FALSE)
+set(WITH_EXTENSIONS FALSE) # Indicates if 'trusted' Slicer extensions should be
+                          # built, tested, packaged and uploaded.
+set(CTEST_BUILD_CONFIGURATION "Release")
+set(CTEST_TRACK_SUFFIX "-ITKv4")
+
+set(CTEST_INCLUDED_SCRIPT_NAME ${HOSTNAME}_slicer_common.cmake)
+include(${CTEST_SCRIPT_DIRECTORY}/${CTEST_INCLUDED_SCRIPT_NAME})
+
+set(SCRIPT_MODE "nightly") # "experimental", "continuous", "nightly"
+
+# Additional CMakeCache options
+# For example:
+#   ADDITIONAL_{C/CXX}_FLAGS: /MP -> Multi process build with MSVC
+#
+set(ADDITIONAL_CMAKECACHE_OPTION "
+  ADDITIONAL_C_FLAGS:STRING=/MP
+  ADDITIONAL_CXX_FLAGS:STRING=/MP
+  Slicer_USE_PYTHONQT:BOOL=ON
+  Slicer_USE_PYTHONQT_WITH_TCL:BOOL=ON
+  Slicer_BUILD_CLI:BOOL=ON
+  ITK_VERSION_MAJOR:STRING=4
+")
+set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}${CTEST_TRACK_SUFFIX}-PythonQt-With-Tcl-CLI")
+#
+# Project specific properties
+#
+set(CTEST_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer-5")
+#set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer-build-${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE}")
+set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/Slicer-5-build")
+
+file(WRITE "${CTEST_DASHBOARD_ROOT}/Slicer-5-build - Slicer-build-${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE}.txt" "Generated by ${CTEST_SCRIPT_NAME}")
+
+# List of test that should be explicitly disabled on this machine
+set(TEST_TO_EXCLUDE_REGEX "qMRMLLayoutManagerTest3")
+# set any extra environment variables here
+if(UNIX)
+  set(ENV{DISPLAY} ":0")
+endif()
+##########################################
+# WARNING: DO NOT EDIT BEYOND THIS POINT #
+##########################################
+set(CTEST_NOTES_FILES "${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}")
+#
+# Project specific properties
+#
+set(CTEST_PROJECT_NAME "Slicer4")
+set(CTEST_BUILD_NAME "${MY_OPERATING_SYSTEM}-${MY_COMPILER}-${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}")
+#
+# Display build info
+#
+message("site name: ${CTEST_SITE}")
+message("build name: ${CTEST_BUILD_NAME}")
+message("script mode: ${SCRIPT_MODE}")
+message("coverage: ${WITH_COVERAGE}, memcheck: ${WITH_MEMCHECK}")
+ 
+#
+# Download and include dashboard driver script 
+#
+set(url http://svn.slicer.org/Slicer4/trunk/CMake/SlicerDashboardDriverScript.cmake)
+set(dest ${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}.driver)
+download_file(${url} ${dest})
+INCLUDE(${dest})

commit b105e74c4ce5d4da87f2929ae794958adf3d234b

factory-south-win7: Ensure CTEST_BUILD_FLAGS is not reset
This commit fixes a regression originally introduced in 549b599

factory-south-win7_common.cmake
@@ -7,8 +7,6 @@ set(CTEST_SITE            "${HOSTNAME}.kitware") # for example: mymachine.kitwar
 get_filename_component(CTEST_SCRIPT_NAME_WE ${CTEST_SCRIPT_NAME} NAME_WE)
 set(CTEST_LOG_FILE        "C:/D/Logs/${CTEST_SCRIPT_NAME_WE}.txt")
 
-set(CTEST_BUILD_FLAGS "")
-
 #find_program(CTEST_GIT_COMMAND NAMES git)
 set(CTEST_GIT_COMMAND "C:/Program Files (x86)/Git/bin/git.exe")
 #find_program(CTEST_SVN_COMMAND NAMES svn)