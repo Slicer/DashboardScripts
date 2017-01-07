569729bb0e7298d12e47d6694ca8920fc5e4f0e4
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-01-06 21:39:45 -0500

    Add overload scripts

---
+++ overload_slicer_common.cmake
@@ -0,0 +1,30 @@
+
+message("-- Including ${CTEST_INCLUDED_SCRIPT_NAME}")
+
+set(CTEST_TEST_TIMEOUT 900) # 15mins
+
+string(TOLOWER "${MY_COMPILER}" _lc_compiler)
+
+if(NOT DEFINED QT_QMAKE_EXECUTABLE)
+  set(QT_QMAKE_EXECUTABLE   "D:/D/Support/qt-${MY_QT_VERSION}-${MY_BITNESS}-${_lc_compiler}-rel/bin/qmake.exe")
+endif()
+
+set(MIDAS_PACKAGE_URL http://slicer.kitware.com/midas3)
+set(SLICER_PACKAGES_CREDENTIAL_FILE "D:/D/Support/Kitware-SlicerPackagesCredential.cmake")
+
+#-----------------------------------------------------------------------------
+# Display variables
+#-----------------------------------------------------------------------------
+foreach(var CTEST_TEST_TIMEOUT MY_QT_VERSION QT_QMAKE_EXECUTABLE
+            MIDAS_PACKAGE_URL SLICER_PACKAGES_CREDENTIAL_FILE
+            )
+  message("-- ${var}: ${${var}}")
+endforeach()
+
+#-----------------------------------------------------------------------------
+# Common settings
+#-----------------------------------------------------------------------------
+set(CTEST_INCLUDED_SCRIPT_NAME ${HOSTNAME}_common.cmake)
+include(${SLICER_PACKAGES_CREDENTIAL_FILE})
+include(${CTEST_SCRIPT_DIRECTORY}/${CTEST_INCLUDED_SCRIPT_NAME})
+
