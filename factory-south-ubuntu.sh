6c551e956de4d4c1b2953662ce37e9a5bb2c602b
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-10-17 22:30:33 -0400

    factory-south-ubuntu: Disable doxygen and rename nightly script

--- factory-south-ubuntu.sh
+++ factory-south-ubuntu.sh
@@ -1,7 +1,7 @@
 export DISPLAY=:0.0 # just DISPLAY=:0.0 without export is not enough
 
 # Nightly build of slicer (Documentation & Package)
-/home/kitware/Dashboards/Support/cmake-3.9.0-Linux-x86_64/bin/ctest -S /home/kitware/Dashboards/DashboardScripts/factory-south-ubuntu-64bits_slicer4_release_nightly_doc.cmake -VV -O /home/kitware/Dashboards/Logs/factory-south-ubuntu-64bits_slicer4_release_nightly_doc.log
+/home/kitware/Dashboards/Support/cmake-3.9.0-Linux-x86_64/bin/ctest -S /home/kitware/Dashboards/DashboardScripts/factory-south-ubuntu-64bits_slicer4_release_nightly.cmake -VV -O /home/kitware/Dashboards/Logs/factory-south-ubuntu-64bits_slicer4_release_nightly.log
 
 # Push generated documentation archives
 (cd /home/kitware/Dashboards/Doxygen && rsync ./Slicer-cpp.tar.gz kitware@public:./Slicer_Doc/)
