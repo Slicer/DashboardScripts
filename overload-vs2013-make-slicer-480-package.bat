8bb69794cef75783f2d2e556a82e25607018c93d
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-10-18 05:22:52 -0400

    overload-vs2013: Fix path to CMake

--- overload-vs2013-make-slicer-480-package.bat
+++ overload-vs2013-make-slicer-480-package.bat
@@ -1,4 +1,4 @@
 
 REM Build Slicer 4.8.0 package using vs2013 64bits
-"D:\cmake-3.9.0\bin\ctest.exe" -S "D:\D\DashboardScripts\overload-vs2013-slicer_480_release_package.cmake" -C Release -V -VV -O D:\D\Logs\overload-vs2013-slicer_480_release_package.txt
+"C:\cmake-3.9.0\bin\ctest.exe" -S "D:\D\DashboardScripts\overload-vs2013-slicer_480_release_package.cmake" -C Release -V -VV -O D:\D\Logs\overload-vs2013-slicer_480_release_package.txt
 
