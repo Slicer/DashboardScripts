16380e35e68002424cec740d7a17e3cbda01ac1b
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-10-17 22:05:34 -0400

    factory: Remove obsolete continuous scripts

--- factory-south-win7-vs2013-64bits_slicer4_release_continuous.bat
+++
@@ -1,1 +0,0 @@
-"C:\D\Support\CMake-3.5.0-rc3\bin\ctest.exe" -S factory-south-win7-vs2013-64bits_slicer4_release_continuous.cmake -C Release -V -VV -O "C:\D\Logs\factory-south-win7-vs2013-64bits_slicer4_release_continuous.txt"
--- factory-south-win7-vs2013-64bits_slicerextensions_release_continuous.bat
+++
@@ -1,1 +0,0 @@
-"C:\D\Support\CMake-3.5.0-rc3\bin\ctest.exe" -S factory-south-win7-vs2013-64bits_slicerextensions_release_continuous.cmake -C Release -V -VV -O "C:\D\Logs\factory-south-win7-vs2013-64bits_slicerextensions_release_continuous.txt"
