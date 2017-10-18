5de85272ed5a16ac11ab0c166681afb50c2ef1a8
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-10-17 22:07:54 -0400

    factory: Remove obsolete Slicer 4.6.0 scripts

--- factory-south-ubuntu-64bits_make-slicer-460-package.sh
+++
@@ -1,9 +0,0 @@
-export DISPLAY=:0.0 # just DISPLAY=:0.0 without export is not enough
-
-HOSTNAME=factory-south-ubuntu
-DASHBOARD_SCRIPTS_DIR=/home/kitware/Dashboards/DashboardScripts
-LOG_DIR=/home/kitware/Dashboards/Logs
-PACKAGE_VERSION=460
-
-# Package build of slicer
-/home/kitware/Dashboards/Support/cmake-3.5.0-rc3-Linux-x86_64/bin/ctest -S $DASHBOARD_SCRIPTS_DIR/${HOSTNAME}-64bits_slicer_${PACKAGE_VERSION}_release_package.cmake -VV -O $LOG_DIR/${HOSTNAME}-64bits_slicer_${PACKAGE_VERSION}_release_package.txt
