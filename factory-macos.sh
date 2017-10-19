b97dbb01cae66be15b8f1ef47f6148a9fb5c4ad3
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-10-19 00:17:49 -0400

    factory-macos: Remove obsolete build of Slicer ITK and CDash@home used by ITK
    Acked-by: Matt McCormick <matt.mccormick@kitware.com>

--- factory-macos.sh
+++ factory-macos.sh
@@ -11,9 +11,3 @@ export PATH=:/usr/local/git/bin:$PATH
 # Nightly build of slicer 4.8 extensions
 /Users/kitware/Dashboards/Support/CMake-3.9.0.app/Contents/bin/ctest -S /Users/kitware/DashboardScripts/factory-macos-slicerextensions_48_release_nightly.cmake -VV -O /Users/kitware/Dashboards/Logs/factory-macos-slicerextensions_48_release_nightly.log
 
-# Nightly build of Slicer's ITK
-/Users/kitware/Dashboards/Support/CMake-3.9.0.app/Contents/bin/ctest -S /Users/kitware/DashboardScripts/factory_slicer_itk_shared.cmake -V
-
-# CDash @home for 18 hours
-cd /Users/kitware/Dashboards/Client/
-/Users/kitware/Dashboards/Support/CMake-3.9.0.app/Contents/bin/ctest -S /Users/kitware/Dashboards/Client/factory.kitware.ctest -V
