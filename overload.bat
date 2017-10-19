14579d574cc37283f51e4ed48e4d7c6244607645
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-10-19 00:30:15 -0400

    overload: Add comments, display IS_WEEKEND value when script starts

--- overload.bat
+++ overload.bat
@@ -1,18 +1,17 @@
 @ECHO OFF
 
-REM Nightly build of slicer vs2013 64bits
-::echo "Nightly build of slicer vs2013 64bits"
-call :fastdel "D:\D\N\Slicer-1-build"
-"C:\cmake-3.9.0\bin\ctest.exe" -S "D:\D\DashboardScripts\overload-vs2013-slicer_release_nightly.cmake" -C Release -VV -O D:\D\Logs\overload-vs2013-slicer_release_nightly.txt
-
-:: See http://serverfault.com/questions/94824/finding-day-of-week-in-batch-file-windows-server-2008
-:: and http://stackoverflow.com/a/14882478/1539918
-for /f "skip=2 tokens=2 delims=," %%a in ('wmic path win32_localtime get dayofweek /format:csv') do set DAYOFWEEK=%%a
-
+:: ----------------------------------------------------------------------------
+:: Set IS_WEEKEND
+:: ----------------------------------------------------------------------------
 :: Since nightly windows build starts after midnight, the following weekday are associated with weekend build:
 ::  6 -> Saturday
 ::  7 -> Sunday
 echo DAYOFWEEK[%DAYOFWEEK%]
+
+:: Get day of the week
+:: See http://serverfault.com/questions/94824/finding-day-of-week-in-batch-file-windows-server-2008
+:: and http://stackoverflow.com/a/14882478/1539918
+for /f "skip=2 tokens=2 delims=," %%a in ('wmic path win32_localtime get dayofweek /format:csv') do set DAYOFWEEK=%%a
 
 set IS_WEEKEND=0
 ::if "%DAYOFWEEK%"=="6" set IS_WEEKEND=1
@@ -20,6 +19,16 @@ if "%DAYOFWEEK%"=="7" set IS_WEEKEND=1
 
 echo IS_WEEKEND[%IS_WEEKEND%]
 
+:: ----------------------------------------------------------------------------
+:: Build Slicer Nightly
+:: ----------------------------------------------------------------------------
+::echo "Nightly build of slicer vs2013 64bits"
+call :fastdel "D:\D\N\Slicer-1-build"
+"C:\cmake-3.9.0\bin\ctest.exe" -S "D:\D\DashboardScripts\overload-vs2013-slicer_release_nightly.cmake" -C Release -VV -O D:\D\Logs\overload-vs2013-slicer_release_nightly.txt
+
+:: ----------------------------------------------------------------------------
+:: Build Slicer Extensions
+:: ----------------------------------------------------------------------------
 if "%IS_WEEKEND%"=="1" (
 ::  call :fastdel "D:\D\N\S480-E-b"
 ::  CALL :overload-vs2013-slicerextensions_48_release_nightly
@@ -32,11 +41,16 @@ if "%IS_WEEKEND%"=="1" (
 ::  CALL :overload-vs2013-slicerextensions_48_release_nightly
 )
 
-:: Add here other jobs
 
 :: force execution to quit at the end of the "main" logic
 EXIT /B %ERRORLEVEL%
 
+
+:: ----------------------------------------------------------------------------
+:: Functions
+:: ----------------------------------------------------------------------------
+
+:: ----------------------------------------------------------------------------
 :: a function to run Nightly extension dashboard
 :overload-vs2013-slicerextensions_release_nightly
 REM Nightly build of slicer extensions vs2013 64bits
@@ -44,7 +58,7 @@ REM Nightly build of slicer extensions vs2013 64bits
 "C:\cmake-3.9.0\bin\ctest.exe" -S "D:\D\DashboardScripts\overload-vs2013-slicerextensions_release_nightly.cmake" -C Release -VV -O D:\D\Logs\overload-vs2013-slicerextensions_release_nightly.txt
 EXIT /B 0
 
-
+:: ----------------------------------------------------------------------------
 :: a function to run Release extension dashboard
 :overload-vs2013-slicerextensions_48_release_nightly
 REM Nightly build of slicer 4.8 extensions vs2013 64bits
