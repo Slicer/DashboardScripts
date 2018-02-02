8f864b3fe8ff952d727b0109b44f079e46819231
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-02-02 16:30:26 -0500

    factory-macos: Disable Slicer nightly build

--- factory-macos.sh
+++ factory-macos.sh
@@ -3,11 +3,11 @@ export DISPLAY=:0.0 # just DISPLAY=:0.0 without export is not enough
 export PATH=:/usr/local/git/bin:$PATH
 
 # Nightly build of slicer
-/Users/kitware/Dashboards/Support/CMake-3.9.0.app/Contents/bin/ctest -S /Users/kitware/DashboardScripts/factory-macos-slicer_release_nightly.cmake -VV -O /Users/kitware/Dashboards/Logs/factory-macos-slicer_release_nightly.log
+#/Users/kitware/Dashboards/Support/CMake-3.9.0.app/Contents/bin/ctest -S /Users/kitware/DashboardScripts/factory-macos-slicer_release_nightly.cmake -VV -O /Users/kitware/Dashboards/Logs/factory-macos-slicer_release_nightly.log
 
 # Nightly build of slicer 4.8 extensions
 /Users/kitware/Dashboards/Support/CMake-3.9.0.app/Contents/bin/ctest -S /Users/kitware/DashboardScripts/factory-macos-slicerextensions_48_release_nightly.cmake -VV -O /Users/kitware/Dashboards/Logs/factory-macos-slicerextensions_48_release_nightly.log
 
 # Nightly build of slicer extensions
-/Users/kitware/Dashboards/Support/CMake-3.9.0.app/Contents/bin/ctest -S /Users/kitware/DashboardScripts/factory-macos-slicerextensions_release_nightly.cmake -VV -O /Users/kitware/Dashboards/Logs/factory-macos-slicerextensions_release_nightly.log
+#/Users/kitware/Dashboards/Support/CMake-3.9.0.app/Contents/bin/ctest -S /Users/kitware/DashboardScripts/factory-macos-slicerextensions_release_nightly.cmake -VV -O /Users/kitware/Dashboards/Logs/factory-macos-slicerextensions_release_nightly.log
 
