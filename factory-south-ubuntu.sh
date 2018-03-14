383cfe48462fcadbbd78a9caed2d04a122c0f9f1
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-03-13 20:16:58 -0400

    factory-south-ubuntu: Rename scripts with "preview" and "stable" suffix

--- factory-south-ubuntu.sh
+++ factory-south-ubuntu.sh
@@ -3,12 +3,12 @@
 
 # To facilitate execution of the command below by copy/paste, they do not include variables.
 
-# Nightly build of slicer
-/home/kitware/Dashboards/Support/cmake-3.11.0-rc3-Linux-x86_64/bin/ctest -S /home/kitware/Dashboards/DashboardScripts/factory-south-ubuntu-slicer_release_nightly.cmake -VV -O /home/kitware/Dashboards/Logs/factory-south-ubuntu-slicer_release_nightly.log
+# Slicer "Preview" release
+/home/kitware/Dashboards/Support/cmake-3.11.0-rc3-Linux-x86_64/bin/ctest -S /home/kitware/Dashboards/DashboardScripts/factory-south-ubuntu-slicer_preview_nightly.cmake -VV -O /home/kitware/Dashboards/Logs/factory-south-ubuntu-slicer_preview_nightly.log
 
-# Nightly build of slicer 4.8 extensions
-/home/kitware/Dashboards/Support/cmake-3.11.0-rc3-Linux-x86_64/bin/ctest -S /home/kitware/Dashboards/DashboardScripts/factory-south-ubuntu-slicerextensions_48_release_nightly.cmake -VV -O /home/kitware/Dashboards/Logs/factory-south-ubuntu-slicerextensions_48_release_nightly.log
+# Slicer "Stable" release extensions
+/home/kitware/Dashboards/Support/cmake-3.11.0-rc3-Linux-x86_64/bin/ctest -S /home/kitware/Dashboards/DashboardScripts/factory-south-ubuntu-slicerextensions_stable_nightly.cmake -VV -O /home/kitware/Dashboards/Logs/factory-south-ubuntu-slicerextensions_stable_nightly.log
 
-# Nightly build of slicer extensions
-/home/kitware/Dashboards/Support/cmake-3.11.0-rc3-Linux-x86_64/bin/ctest -S /home/kitware/Dashboards/DashboardScripts/factory-south-ubuntu-slicerextensions_release_nightly.cmake -VV -O /home/kitware/Dashboards/Logs/factory-south-ubuntu-slicerextensions_release_nightly.log
+# Slicer "Preview" release extensions
+/home/kitware/Dashboards/Support/cmake-3.11.0-rc3-Linux-x86_64/bin/ctest -S /home/kitware/Dashboards/DashboardScripts/factory-south-ubuntu-slicerextensions_preview_nightly.cmake -VV -O /home/kitware/Dashboards/Logs/factory-south-ubuntu-slicerextensions_preview_nightly.log
 
