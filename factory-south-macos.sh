d19aabdaa930f559f1bdee241650d3df500cfb26
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-03-13 20:16:37 -0400

    factory-south-macos: Rename scripts with "preview" and "stable" suffix

--- factory-south-macos.sh
+++ factory-south-macos.sh
@@ -1,15 +1,14 @@
 open -a x11
-
 # CMAKE_VERSION=3.11.0-rc3 - This comment is used by the maintenance script to look up the cmake version
 
 # To facilitate execution of the command below by copy/paste, they do not include variables.
 
-# Nightly build of slicer
-/Volumes/Dashboards/Support/CMake-3.11.0-rc3.app/Contents/bin/ctest -S /Volumes/Dashboards/DashboardScripts/factory-south-macos-slicer_release_nightly.cmake -VV -O /Volumes/Dashboards/Logs/factory-south-macos-slicer_release_nightly.log
+# Slicer "Preview" release
+/Volumes/Dashboards/Support/CMake-3.11.0-rc3.app/Contents/bin/ctest -S /Volumes/Dashboards/DashboardScripts/factory-south-macos-slicer_preview_nightly.cmake -VV -O /Volumes/Dashboards/Logs/factory-south-macos-slicer_preview_nightly.log
 
-# Nightly build of slicer 4.8 extensions
-#/Volumes/Dashboards/Support/CMake-3.11.0-rc3.app/Contents/bin/ctest -S /Volumes/Dashboards/DashboardScripts/factory-south-macos-slicerextensions_48_release_nightly.cmake -VV -O /Volumes/Dashboards/Logs/factory-south-macos-slicerextensions_48_release_nightly.log
+# Slicer "Stable" release extensions
+#/Volumes/Dashboards/Support/CMake-3.11.0-rc3.app/Contents/bin/ctest -S /Volumes/Dashboards/DashboardScripts/factory-south-macos-slicerextensions_stable_nightly.cmake -VV -O /Volumes/Dashboards/Logs/factory-south-macos-slicerextensions_stable_nightly.log
 
-# Nightly build of slicer extensions
-/Volumes/Dashboards/Support/CMake-3.11.0-rc3.app/Contents/bin/ctest -S /Volumes/Dashboards/DashboardScripts/factory-south-macos-slicerextensions_release_nightly.cmake -VV -O /Volumes/Dashboards/Logs/factory-south-macos-slicerextensions_release_nightly.log
+# Slicer "Preview" release extensions
+/Volumes/Dashboards/Support/CMake-3.11.0-rc3.app/Contents/bin/ctest -S /Volumes/Dashboards/DashboardScripts/factory-south-macos-slicerextensions_preview_nightly.cmake -VV -O /Volumes/Dashboards/Logs/factory-south-macos-slicerextensions_preview_nightly.log
 
