5ab84211a80ff3d51eadfd3965cfb068688d54be
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-03-14 02:02:57 -0400

    factory-sout-ubuntu: Fix build ensuring newer version of git is found\
    See https://gist.github.com/jcfr/811a23d722bc245fe077e08d897d20e8
    
    This should address error like this one:
    
    Cloning into 'thirdparty/gtest'...
    fatal: unable to access 'https://github.com/google/googletest.git/': gnutls_handshake() failed: A TLS fatal alert has been received.
    Clone of 'https://github.com/google/googletest.git' into submodule path 'thirdparty/gtest' failed
    CMake Error at RapidJSON-prefix/tmp/RapidJSON-gitclone.cmake:93 (message):
      Failed to update submodules in:
      '/home/kitware/Dashboards/Preview/Slicer-0-build/RapidJSON'

--- factory-south-ubuntu.sh
+++ factory-south-ubuntu.sh
@@ -1,4 +1,9 @@
 
+# See https://gist.github.com/jcfr/811a23d722bc245fe077e08d897d20e8
+export LD_LIBRARY_PATH=/usr/local/lib64:/usr/local/lib
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+export PATH=/usr/local/bin:$PATH
+
 # CMAKE_VERSION=3.11.0-rc3 - This comment is used by the maintenance script to look up the cmake version
 
 # To facilitate execution of the command below by copy/paste, they do not include variables.
