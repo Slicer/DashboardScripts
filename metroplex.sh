c543e504128fe203c2f517a9c6f4a6d876cacdf4
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-03-30 20:45:24 -0400

    metroplex: Disable tests and support passing few "run_ctest_with_*" env. vars

--- metroplex.sh
+++ metroplex.sh
@@ -5,9 +5,15 @@ pushd /home/kitware/Dashboards/Slicer > /dev/null 2>&1
 
 # To facilitate execution of the command below by copy/paste, they do not include variables.
 
+run_ctest_with_disable_clean=${run_ctest_with_disable_clean-FALSE}
+run_ctest_with_update=${run_ctest_with_update-TRUE}
+
+# XXX Re-enable testing after slicer/slicer-test images have been updated
+run_ctest_with_test=${run_ctest_with_test-FALSE}
+
 # Slicer 'Preview' release
 slicer-buildenv-qt5-centos7-latest \
-  --args "-e run_ctest_with_test=FALSE" \
+  --args "-e run_ctest_with_disable_clean=${run_ctest_with_disable_clean} -e run_ctest_with_update=${run_ctest_with_update} -e run_ctest_with_test=${run_ctest_with_test}" \
   ctest -S /work/DashboardScripts/metroplex-slicer_preview_nightly.cmake -VV -O /work/Support/Logs/metroplex-slicer_preview_nightly.log
 
 # Slicer 'Stable' release extensions
