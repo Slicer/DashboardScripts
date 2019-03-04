bae66c5188eb4d482e4adadb551ce24405d1e7ee
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2019-03-04 14:16:03 -0500

    factory-south-macos, overload: Update from CMake 3.12.2 to 3.13.4

--- factory-south-macos.sh
+++ factory-south-macos.sh
@@ -1,22 +1,22 @@
-# CMAKE_VERSION=3.12.2 - This comment is used by the maintenance script to look up the cmake version
+# CMAKE_VERSION=3.13.4 - This comment is used by the maintenance script to look up the cmake version
 
 # To facilitate execution of the command below by copy/paste, they do not include variables.
 
 # Slicer 'Preview' release
-/Volumes/Dashboards/Support/CMake-3.12.2.app/Contents/bin/ctest -S /Volumes/Dashboards/DashboardScripts/factory-south-macos-slicer_preview_nightly.cmake -VV -O /Volumes/Dashboards/Logs/factory-south-macos-slicer_preview_nightly.log
+/Volumes/Dashboards/Support/CMake-3.13.4.app/Contents/bin/ctest -S /Volumes/Dashboards/DashboardScripts/factory-south-macos-slicer_preview_nightly.cmake -VV -O /Volumes/Dashboards/Logs/factory-south-macos-slicer_preview_nightly.log
 
 # SlicerSALT 'Preview' release
-/Volumes/Dashboards/Support/CMake-3.12.2.app/Contents/bin/ctest -S /Volumes/Dashboards/DashboardScripts/factory-south-macos-slicersalt_preview_nightly.cmake -VV -O /Volumes/Dashboards/Logs/factory-south-macos-slicersalt_preview_nightly.log
+/Volumes/Dashboards/Support/CMake-3.13.4.app/Contents/bin/ctest -S /Volumes/Dashboards/DashboardScripts/factory-south-macos-slicersalt_preview_nightly.cmake -VV -O /Volumes/Dashboards/Logs/factory-south-macos-slicersalt_preview_nightly.log
 
 # SlicerSALT 'Preview' release - generate package
-/Volumes/Dashboards/Support/CMake-3.12.2.app/Contents/bin/cmake --build /Volumes/Dashboards/Preview/SSALT-0-build/Slicer-build --target package --config Release > /Volumes/Dashboards/Logs/factory-south-macos-slicersalt-generate-package.txt 2>&1
+/Volumes/Dashboards/Support/CMake-3.13.4.app/Contents/bin/cmake --build /Volumes/Dashboards/Preview/SSALT-0-build/Slicer-build --target package --config Release > /Volumes/Dashboards/Logs/factory-south-macos-slicersalt-generate-package.txt 2>&1
 
 # SlicerSALT 'Preview' release - package upload
-/Volumes/Dashboards/Support/CMake-3.12.2.app/Contents/bin/cmake -P /Volumes/Dashboards/DashboardScripts/scripts/slicersalt-upload-package.cmake > /Volumes/Dashboards/Logs/factory-south-macos-slicersalt-upload-package.txt 2>&1
+/Volumes/Dashboards/Support/CMake-3.13.4.app/Contents/bin/cmake -P /Volumes/Dashboards/DashboardScripts/scripts/slicersalt-upload-package.cmake > /Volumes/Dashboards/Logs/factory-south-macos-slicersalt-upload-package.txt 2>&1
 
 # Slicer 'Stable' release extensions
-/Volumes/Dashboards/Support/CMake-3.12.2.app/Contents/bin/ctest -S /Volumes/Dashboards/DashboardScripts/factory-south-macos-slicerextensions_stable_nightly.cmake -VV -O /Volumes/Dashboards/Logs/factory-south-macos-slicerextensions_stable_nightly.log
+/Volumes/Dashboards/Support/CMake-3.13.4.app/Contents/bin/ctest -S /Volumes/Dashboards/DashboardScripts/factory-south-macos-slicerextensions_stable_nightly.cmake -VV -O /Volumes/Dashboards/Logs/factory-south-macos-slicerextensions_stable_nightly.log
 
 # Slicer 'Preview' release extensions
-/Volumes/Dashboards/Support/CMake-3.12.2.app/Contents/bin/ctest -S /Volumes/Dashboards/DashboardScripts/factory-south-macos-slicerextensions_preview_nightly.cmake -VV -O /Volumes/Dashboards/Logs/factory-south-macos-slicerextensions_preview_nightly.log
+/Volumes/Dashboards/Support/CMake-3.13.4.app/Contents/bin/ctest -S /Volumes/Dashboards/DashboardScripts/factory-south-macos-slicerextensions_preview_nightly.cmake -VV -O /Volumes/Dashboards/Logs/factory-south-macos-slicerextensions_preview_nightly.log
 
