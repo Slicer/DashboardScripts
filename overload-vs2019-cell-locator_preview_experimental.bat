4b8933867da9adf6172e6c3c86b27d025cb940b3
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2020-08-12 10:36:49 -0400

    overload: Add cell-locator scripts to build against updated application
    See https://github.com/BICCN/cell-locator/pull/131

---
+++ overload-vs2019-cell-locator_preview_experimental.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+:: CMAKE_VERSION=3.17.2 - This comment is used by the maintenance script to look up the cmake version
+
+:: Disable tests
+set run_ctest_with_test=FALSE
+
+:: ----------------------------------------------------------------------------
+:: Build CellLocator
+:: ----------------------------------------------------------------------------
+::echo "CellLocator 'Preview' release"
+"C:\cmake-3.17.2\bin\ctest.exe" -S "D:\D\DashboardScripts\overload-vs2019-cell-locator_preview_experimental.cmake" -C Release -VV -O D:\D\Logs\overload-vs2019-cell-locator_preview_experimental.txt
+
+::echo "CellLocator 'Preview' release - generate package"
+"C:\cmake-3.17.2\bin\cmake.exe" --build "D:\D\P\CL-0-build/Slicer-build" --target PACKAGE --config Release > D:\D\Logs\overload-vs2019-cell-locator_preview_experimental-generate-package.txt 2>&1
