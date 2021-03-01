daee64317a5f81e63edb80d1ea2469d2f01e3a67
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-03-01 17:55:54 -0500

    computron, factory-south-macos: Update from CMake 3.15.1 to 3.17.2
    Version used on macOS is now consistent with overload dashboard

--- computron.sh
+++ computron.sh
@@ -1,7 +1,7 @@
-# CMAKE_VERSION=3.15.1 - This comment is used by the maintenance script to look up the cmake version
+# CMAKE_VERSION=3.17.2 - This comment is used by the maintenance script to look up the cmake version
 
 # Clear Slicer settings
 rm -rf /Users/svc-dashboard/.config/www.na-mic.org/
 
 # Slicer 'Preview' release
-/D/Support/CMake-3.15.1.app/Contents/bin/ctest -S /D/DashboardScripts/computron-slicer_preview_nightly.cmake -VV -O /D/Logs/computron-slicer_preview_nightly.log
+/D/Support/CMake-3.17.2.app/Contents/bin/ctest -S /D/DashboardScripts/computron-slicer_preview_nightly.cmake -VV -O /D/Logs/computron-slicer_preview_nightly.log
