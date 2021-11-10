c9879876ed337c3c908895628430daf30e7a221a
James Butler <jbutler@sonovol.com>
2021-11-10 15:25:27 -0500

    overload, computron, factory-south-macos: Update from CMake 3.17.2 to 3.22.1
    This resolves uninstall issues of factory-built Slicer versions on Windows. See https://github.com/Slicer/Slicer/issues/4933.

--- computron.sh
+++ computron.sh
@@ -1,10 +1,10 @@
-# CMAKE_VERSION=3.17.2 - This comment is used by the maintenance script to look up the cmake version
+# CMAKE_VERSION=3.22.1 - This comment is used by the maintenance script to look up the cmake version
 
 # Clear Slicer settings
 rm -rf /Users/svc-dashboard/.config/www.na-mic.org/
 
 # Slicer 'Preview' release
-/D/Support/CMake-3.17.2.app/Contents/bin/ctest -S /D/DashboardScripts/computron-slicer_preview_nightly.cmake -VV -O /D/Logs/computron-slicer_preview_nightly.log
+/D/Support/CMake-3.22.1.app/Contents/bin/ctest -S /D/DashboardScripts/computron-slicer_preview_nightly.cmake -VV -O /D/Logs/computron-slicer_preview_nightly.log
 
 # Slicer 'Preview' release extensions
-/D/Support/CMake-3.17.2.app/Contents/bin/ctest -S /D/DashboardScripts/computron-slicerextensions_preview_nightly.cmake -VV -O /D/Logs/factory-south-macos-slicerextensions_preview_nightly.log
+/D/Support/CMake-3.22.1.app/Contents/bin/ctest -S /D/DashboardScripts/computron-slicerextensions_preview_nightly.cmake -VV -O /D/Logs/factory-south-macos-slicerextensions_preview_nightly.log
