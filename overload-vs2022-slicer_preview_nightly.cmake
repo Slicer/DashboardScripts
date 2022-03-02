7725ba39efd526b6ea903f8ceff705c48f3da602
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-03-02 17:08:06 -0500

    overload: Attempt to fix extension build using shorter dir basename Slicer -> S
    See https://github.com/Slicer/Slicer/issues/5269 and https://github.com/IGSIO/SlicerIGSIO/issues/17

--- overload-vs2022-slicer_preview_nightly.cmake
+++ overload-vs2022-slicer_preview_nightly.cmake
@@ -36,7 +36,7 @@ dashboard_set(Qt5_DIR             "D:/Support/Qt2/${QT_VERSION}/msvc2019_64/lib/
 
 #   Source directory : <DASHBOARDS_DIR>/<Slicer_DASHBOARD_SUBDIR>/<Slicer_DIRECTORY_BASENAME>-<Slicer_DIRECTORY_IDENTIFIER>
 #   Build directory  : <DASHBOARDS_DIR>/<Slicer_DASHBOARD_SUBDIR>/<Slicer_DIRECTORY_BASENAME>-<Slicer_DIRECTORY_IDENTIFIER>-build
-dashboard_set(Slicer_DIRECTORY_BASENAME   "Slicer")
+dashboard_set(Slicer_DIRECTORY_BASENAME   "S")
 dashboard_set(Slicer_DASHBOARD_SUBDIR     "${Slicer_RELEASE_TYPE}")
 dashboard_set(Slicer_DIRECTORY_IDENTIFIER "0")        # Set to arbitrary integer to distinguish different Experimental/Preview release build
                                                       # Set to Slicer version XYZ for Stable release build
