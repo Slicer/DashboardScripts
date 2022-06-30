84ec25c2b3a6705b564f2e4c6623c0b352480c7f
Sam Horvath <sam.horvath@kitware.com>
2022-06-30 12:06:02 -0400

    overload: fix msvc version for Qt 5.15

--- overload-vs2022-slicersalt_preview_nightly.cmake
+++ overload-vs2022-slicersalt_preview_nightly.cmake
@@ -32,7 +32,7 @@ dashboard_set(Slicer_BUILD_CLI    ON)
 dashboard_set(Slicer_USE_PYTHONQT ON)
 
 dashboard_set(QT_VERSION          "5.15.2")
-dashboard_set(Qt5_DIR             "D:/Support/Qt2/${QT_VERSION}/msvc2015_64/lib/cmake/Qt5")
+dashboard_set(Qt5_DIR             "D:/Support/Qt2/${QT_VERSION}/msvc2019_64/lib/cmake/Qt5")
 
 #   Source directory : <DASHBOARDS_DIR>/<Slicer_DASHBOARD_SUBDIR>/<Slicer_DIRECTORY_BASENAME>-<Slicer_DIRECTORY_IDENTIFIER>
 #   Build directory  : <DASHBOARDS_DIR>/<Slicer_DASHBOARD_SUBDIR>/<Slicer_DIRECTORY_BASENAME>-<Slicer_DIRECTORY_IDENTIFIER>-build
