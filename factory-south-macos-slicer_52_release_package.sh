84a79ac8f442e4f009a3b6b2408325b8e2b2fdf0
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-11-24 22:53:50 -0500

    factory-south-macos: Delete saved settings, crash reporter reports, application states
    This should help re-run tests more deterministically

--- factory-south-macos-slicer_52_release_package.sh
+++ factory-south-macos-slicer_52_release_package.sh
@@ -1,5 +1,14 @@
 # CMAKE_VERSION=NA - This comment is used by the maintenance script to look up the cmake version
 
+# Clear Slicer settings
+rm -rf /Users/kitware/.config/www.na-mic.org/
+
+# Clear reports to help avoid "The last time you opened ... Do you want to try to reopen its windows again?" dialog
+rm -rf /Users/kitware/Library/Application\ Support/CrashReporter/*
+
+# Clear "Saved Application State" specific to Slicer
+rm -rf /Users/kitware/Library/Saved\ Application\ State/org.slicer.slicer.savedState/
+
 # Slicer 'Stable' release
 /Volumes/D/Support/CMake-3.22.1.app/Contents/bin/ctest -S /Volumes/D/DashboardScripts/factory-south-macos-slicer_52_release_package.cmake -VV -O /Volumes/D/Logs/factory-south-macos-slicer_52_release_package.log
 
