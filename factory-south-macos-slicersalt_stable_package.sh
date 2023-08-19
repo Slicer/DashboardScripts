9e88761312c38ef45df522d64bff5eedee614ba5
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-08-19 06:27:03 -0400

    slicersalt 4.0: Update release scripts to include "stable" instead of "40_release"

---
+++ factory-south-macos-slicersalt_stable_package.sh
@@ -0,0 +1,4 @@
+# CMAKE_VERSION=NA - This comment is used by the maintenance script to look up the cmake version
+
+# SlicerSALT 'Stable' release
+/Volumes/D/Support/CMake-3.22.1.app/Contents/bin/ctest -S /Volumes/D/DashboardScripts/factory-south-macos-slicersalt_stable_package.cmake -VV -O /Volumes/D/Logs/factory-south-macos-slicersalt_stable_package.log
