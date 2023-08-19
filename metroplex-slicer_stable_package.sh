60ff76c5e06edf85771641f36bf61413f664c65c
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-08-19 06:15:13 -0400

    slicer 5.4: Update stable scripts for overload and metroplex

--- metroplex-slicer_stable_package.sh
+++ metroplex-slicer_stable_package.sh
@@ -19,7 +19,7 @@ rm -rf /home/kitware/Dashboards/Slicer/Stable/Slicer-0-build
 rm -rf /home/kitware/Dashboards/Slicer/Stable/S-0-E-b
 
 # Slicer 'Stable' release
-time /home/kitware/bin/slicer-buildenv-qt5-centos7-slicer-5.2 \
+time /home/kitware/bin/slicer-buildenv-qt5-centos7-slicer-5.4 \
   --args "${docker_args}" \
   ctest -S /work/DashboardScripts/metroplex-slicer_stable_package.cmake -VV -O /work/Logs/metroplex-slicer_stable_package.log
 
@@ -29,7 +29,7 @@ time cp -rp \
   /home/kitware/Dashboards/Slicer/Stable/Slicer-0-build/python-install/lib/python3.9/site-packages.bkp/
 
 # Slicer 'Stable' release extensions
-time /home/kitware/bin/slicer-buildenv-qt5-centos7-slicer-5.2 \
+time /home/kitware/bin/slicer-buildenv-qt5-centos7-slicer-5.4 \
    --args "${docker_args}" \
    ctest -S /work/DashboardScripts/metroplex-slicerextensions_stable_nightly.cmake -VV -O /work/Logs/metroplex-slicerextensions_stable_nightly.log
 
