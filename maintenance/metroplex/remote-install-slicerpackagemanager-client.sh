7d313bb06694fe8ca032109d75866b7629f5ba48
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-08-22 18:58:39 -0400

    remote-install-slicerpackagemanager-client/metroplex: Remove host install
    Follow-up of 6009cee576 (remote-install-slicerpackagemanager-client: Install
    package from PyPI) removing support for using the client directly on the
    host where only Python 3.6 is available.

--- maintenance/metroplex/remote-install-slicerpackagemanager-client.sh
+++ maintenance/metroplex/remote-install-slicerpackagemanager-client.sh
@@ -10,8 +10,7 @@ source $dashboard_dir/../common/remote_execute.sh
 #
 # The venv are created to enable the use of slicer_package_manager in the following
 # contexts:
-# (a) the remote system
-# (b) the build-env docker images installed on the remote system
+# * the build-env docker images installed on the remote system
 #
 # It then performs the following tasks on the remote:
 #
@@ -23,29 +22,8 @@ source $dashboard_dir/../common/remote_execute.sh
 
 #------------------------------------------------------------------------------
 #
-# Download source
-#
-#------------------------------------------------------------------------------
-
-#------------------------------------------------------------------------------
-# Generate script
-#
-script_name=$(basename $0)-download-source
-cat << REMOTE_SCRIPT_EOF > /tmp/$script_name
-
-set -ex
-
-[[ \$(hostname) != "${remote_hostname}" ]] && exit 1 || true
-
-REMOTE_SCRIPT_EOF
-
-#------------------------------------------------------------------------------
-# Execute script on remote
-#
-remote_execute /tmp/$script_name
-
-#------------------------------------------------------------------------------
 # Build-env install(s)
+#
 #------------------------------------------------------------------------------
 
 SLICER_PREVIEW_ENV_NAME=qt5-centos7
@@ -79,20 +57,12 @@ set -ex
 
 cd $remote_build_env_root_dir
 
-if [[ ! -d ${venv_name} ]]; then
+if [[ ! -d ${remote_support_dir}/${venv_name} ]]; then
   $remote_build_env_script $remote_in_build_env_python -m venv ${remote_in_build_env_support_dir}/${venv_name}
 fi
 
 $remote_build_env_script $remote_in_build_env_pip install --upgrade pip
-$remote_build_env_script $remote_in_build_env_pip install --upgrade setuptools
-$remote_build_env_script $remote_in_build_env_pip install -U bson girder_client
-$remote_build_env_script $remote_in_build_env_pip install -U ${remote_in_build_env_support_dir}/${proj_name}/python_client
-
-# workaround missing _sqlite3 python module
-$remote_build_env_script $remote_in_build_env_pip install diskcache
-$remote_build_env_script /usr/bin/rm -rf ${remote_in_build_env_support_dir}/${venv_name}/lib/python3.6/site-packages/diskcache
-$remote_build_env_script /usr/bin/mkdir ${remote_in_build_env_support_dir}/${venv_name}/lib/python3.6/site-packages/diskcache
-$remote_build_env_script /usr/bin/touch  ${remote_in_build_env_support_dir}/${venv_name}/lib/python3.6/site-packages/diskcache/
+$remote_build_env_script $remote_in_build_env_pip install -U slicer-package-manager-client
 
 $remote_build_env_script $remote_in_build_env_client --help
 
@@ -109,37 +79,4 @@ remote_execute /tmp/$script_name
 #
 #------------------------------------------------------------------------------
 
-#------------------------------------------------------------------------------
-# Set script properties
-#
-venv_name="${proj_name}-venv-system"
-remote_python="/home/kitware/.nix-profile/bin/python3.6"
-remote_pip="${remote_support_dir}/${venv_name}/bin/pip"
-remote_client="${remote_support_dir}/${venv_name}/bin/slicer_package_manager_client"
-
-#------------------------------------------------------------------------------
-# Generate script
-#
-script_name=$(basename $0)-system-install
-cat << REMOTE_SCRIPT_EOF > /tmp/$script_name
-
-set -ex
-
-[[ \$(hostname) != "${remote_hostname}" ]] && exit 1 || true
-
-cd $remote_support_dir
-
-if [[ ! -d ${venv_name} ]]; then
-  ${remote_python} -m venv ${venv_name}
-fi
-
-$remote_pip install -U slicer-package-manager-client
-
-$remote_client --help
-
-REMOTE_SCRIPT_EOF
-
-#------------------------------------------------------------------------------
-# Execute script on remote
-#
-remote_execute /tmp/$script_name
+# As of August 2023, support for using the client directly on the host is not available anymore.
