e33bcf0b7f3eeafcd9152d6cc8bf7cefb92a423e
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-08-22 20:43:40 -0400

    clean-nightly-slicer-package-manager: Revisit authentication

--- maintenance/metroplex/clean-nightly-slicer-package-manager-cron.sh
+++ maintenance/metroplex/clean-nightly-slicer-package-manager-cron.sh
@@ -7,6 +7,11 @@ PROG=$(basename $0)
 script_dir=$(cd $(dirname $0) || exit 1; pwd)
 
 #-------------------------------------------------------------------------------
+# Read key outside of the build environment working directory
+api_key_file=/home/kitware/Support/Kitware-SlicerPackagesManager-ApiKey
+api_key=$(head -n1 ${api_key_file})
+
+#-------------------------------------------------------------------------------
 SLICER_PREVIEW_ENV_NAME=qt5-centos7
 SLICER_PREVIEW_ENV_VERSION=latest
 
@@ -15,4 +20,7 @@ build_env_script=/home/kitware/bin/slicer-buildenv-${SLICER_PREVIEW_ENV_NAME}-${
 
 cd /home/kitware/Dashboards/Slicer
 
-$build_env_script bash /work/DashboardScripts/maintenance/metroplex/clean-nightly-slicer-package-manager.sh -v
+$build_env_script \
+  --args "-e GIRDER_API_KEY=${api_key}" \
+  bash \
+    /work/DashboardScripts/maintenance/metroplex/clean-nightly-slicer-package-manager.sh -v
