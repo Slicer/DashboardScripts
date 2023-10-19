db341d222a3526dc35858ffcf6490a328de082d7
Sam Horvath <sam.horvath@kitware.com>
2023-10-19 16:39:23 -0400

    factory-south: remove build and scripts for SlicerSALT

--- factory-south-macos.sh
+++ factory-south-macos.sh
@@ -12,18 +12,6 @@ rm -rf /Users/kitware/Library/Application\ Support/CrashReporter/*
 # Clear "Saved Application State" specific to Slicer
 rm -rf /Users/kitware/Library/Saved\ Application\ State/org.slicer.slicer.savedState/
 
-# Clear SlicerSALT settings
-rm -rf /Users/kitware/.config/kitware.com/
-
-# SlicerSALT 'Preview' release
-/Volumes/D/Support/CMake-3.22.1.app/Contents/bin/ctest -S /Volumes/D/DashboardScripts/factory-south-macos-slicersalt_preview_nightly.cmake -VV -O /Volumes/D/Logs/factory-south-macos-slicersalt_preview_nightly.log
-
-# SlicerSALT 'Preview' release - generate package
-/Volumes/D/Support/CMake-3.22.1.app/Contents/bin/cmake --build /Volumes/D/P/SSALT-0-build/Slicer-build --target package --config Release > /Volumes/D/Logs/factory-south-macos-slicersalt-generate-package.txt 2>&1
-
-# SlicerSALT 'Preview' release - package upload
-/Volumes/D/Support/CMake-3.22.1.app/Contents/bin/cmake -P /Volumes/D/DashboardScripts/scripts/slicersalt-upload-package.cmake > /Volumes/D/Logs/factory-south-macos-slicersalt-upload-package.txt 2>&1
-
-#Medical Team Dashboard
+# Medical Team Dashboard
 /Volumes/D/Med/MedicalTeamDashboardScripts/factory-macos-south.sh > /Volumes/D/Med/Logs/factory-macos-south.log 2>&1
 
