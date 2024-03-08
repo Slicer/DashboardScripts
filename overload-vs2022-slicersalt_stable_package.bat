1bd89ffaf8857f0ba7bb53e588f07c78f0de5abf
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2024-03-08 14:44:32 -0500

    Remove windows slicersalt scripts now managed as regular Kitware custom app

--- overload-vs2022-slicersalt_stable_package.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-:: CMAKE_VERSION=NA - This comment is used by the maintenance script to look up the cmake version
-
-:: ----------------------------------------------------------------------------
-:: Build Slicer
-:: ----------------------------------------------------------------------------
-::echo "Slicer 'Stable' release"
-"C:\cmake-3.22.1\bin\ctest.exe" -S "D:\D\DashboardScripts\overload-vs2022-slicersalt_stable_package.cmake" -C Release -VV -O D:\D\Logs\overload-vs2022-slicersalt_stable_package.txt
