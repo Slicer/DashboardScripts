02862fb1be10f621aae3431be36e80e7be6cec2c
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2024-03-08 14:44:32 -0500

    Remove slicersalt scripts now managed as regular Kitware custom app

--- metroplex.sh
+++ metroplex.sh
@@ -78,41 +78,6 @@ if [ $with_itk_dashboard == 1 ]; then
   time /home/kitware/Dashboards/KWDashboardScripts/metroplex.sh > /home/kitware/Dashboards/Logs/metroplex.log 2>&1
 fi
 
-#-------------------------------------------------------------------------------
-# Download and patch the slicer-buildenv-qt5-centos7:slicer-4.11-2018.10.17 image
-SLICER_SALT_ENV_NAME=qt5-centos7
-SLICER_SALT_ENV_VERSION=latest
-
-# Download build environment
-slicer_salt_script=/home/kitware/bin/slicer-buildenv-${SLICER_SALT_ENV_NAME}-${SLICER_SALT_ENV_VERSION}
-if [[ ! -f ${slicer_salt_script} ]]; then
-  docker run --rm slicer/buildenv-${SLICER_SALT_ENV_NAME}:${SLICER_SALT_ENV_VERSION} > $slicer_salt_script
-  chmod +x $slicer_salt_script
-fi
-
-# Update build environment
-$slicer_salt_script update
-# HACK: Workaround limitation of entrypoint.sh (see https://github.com/Slicer/SlicerBuildEnvironment/issues/16)
-sed -i ${slicer_salt_script} -e "s/slicer\/buildenv-${SLICER_SALT_ENV_NAME}:latest/slicer\/buildenv-${SLICER_SALT_ENV_NAME}:${SLICER_SALT_ENV_VERSION}/"
-
-# SlicerSALT dashboard settings
-slicersalt_docker_args="-e run_ctest_with_disable_clean=${run_slicersalt_ctest_with_disable_clean-FALSE}"
-slicersalt_docker_args+=" -e run_ctest_with_update=${run_slicersalt_ctest_with_update-TRUE}"
-slicersalt_docker_args+=" -e run_ctest_with_test=${run_slicersalt_ctest_with_test-TRUE}" 
-docker_args+=" -e QT_QPA_PLATFORM=offscreen"  #Allows headlesss testing with some graphics failures
-
-# SlicerSALT 'Preview' release
-time ${slicer_salt_script} \
-  --args "${slicersalt_docker_args}" \
-  ctest -S /work/DashboardScripts/metroplex-slicersalt_preview_nightly.cmake -VV -O /work/Logs/metroplex-slicersalt_preview_nightly.log
-
-# SlicerSALT 'Preview' release - generate package
-time ${slicer_salt_script} \
-  cmake --build /work/Preview/SSALT-0-build/Slicer-build/ --target package --config Release > /home/kitware/Dashboards/Logs/metroplex-slicersalt-generate-package.txt 2>&1
-
-# SlicerSALT 'Preview' release - package upload
-/home/kitware/.nix-profile/bin/cmake -P /home/kitware/Dashboards/Slicer/DashboardScripts/scripts/slicersalt-upload-package.cmake > /home/kitware/Dashboards/Logs/metroplex-slicersalt-upload-package.txt 2>&1
-
 #Medical Team Dashboard
 time /home/kitware/Dashboards/Medical/MedicalTeamDashboardScripts/metroplex.sh > /home/kitware/Dashboards/Medical/Logs/metroplex.log 2>&1
 
